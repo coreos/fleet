1c898fb261232dfaa1d2660000cd699a39110481
Brandon Philips <brandon.philips@ifup.co>
2013-10-12 10:07:47 -0700

    fix(muffins): cleanup code formatting

--- muffins.go
+++ muffins.go
@@ -6,7 +6,12 @@ import (
 )
 
 func main() {
+	script := []string{"/bin/sh", "-c",
+		"while true; do echo goodbye world; sleep 1; done"}
+
 	systemd := dbus.New()
-	jobid, err := systemd.StartTransientUnit("hello.service", "replace", dbus.PropExecStart([]string{"/bin/sh", "-c", "echo hello world"}, false))
+	jobid, err := systemd.StartTransientUnit("hello.service",
+		"replace",
+		dbus.PropExecStart(script, false))
 	fmt.Println(jobid, err)
 }
