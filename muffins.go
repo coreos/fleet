c48c5ca3c60ecb26e136dd40de25ff7560400132
Brandon Philips <brandon@ifup.co>
2013-10-11 17:25:31 -0700

    feat(muffins): create transient unit

--- muffins.go
+++ muffins.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/coreos/go-systemd/dbus"
 )
 
 func main() {
-	fmt.Println("I am the muffin man")
+	systemd := dbus.New()
+	jobid, err := systemd.StartTransientUnit("hello.service", "replace", dbus.PropExecStart([]string{"/bin/sh", "-c", "echo hello world"}, false))
+	fmt.Println(jobid, err)
 }
