afbc1787795de3ed644b501a4bb983f6d7c7f9d5
Brandon Philips <brandon.philips@ifup.co>
2013-10-12 16:27:22 -0700

    feat(muffins): write all of the keys on an interval

--- muffins.go
+++ muffins.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"path"
+	"time"
 
 	"github.com/coreos/muffins/machine"
 	"github.com/coreos/go-etcd/etcd"
@@ -9,6 +10,14 @@ import (
 )
 
 const keyPrefix = "/core-init/system/"
+// TODO: Make this a variable
+const ttl = 10
+
+type Init struct {
+	Etcd *etcd.Client
+	Systemd *dbus.Conn
+	Machine *machine.Machine
+}
 
 func init() {
 }
@@ -18,29 +27,49 @@ func startUnit() {
 	script := []string{"/bin/sh", "-c",
 		"while true; do echo goodbye world; sleep 1; done"}
 
-	jobid, err := systemd.StartTransientUnit("hello.service",
+	jobid, err := i.Systemd.StartTransientUnit("hello.service",
 		"replace",
 		dbus.PropExecStart(script, false))
 	fmt.Println(jobid, err)
 }
 */
 
-func main() {
-	etcdC := etcd.NewClient(nil)
-	mach := machine.NewMachine("")
-	prefix := path.Join(keyPrefix, mach.BootId)
+// heartbeat ensures that all of the units 
+func (i *Init) StartHeartbeat() {
+	// TODO: Use the new directory TTL in the v2 API instead of
+	// heartbeating all of the units
+	interval := ttl / 2.0
+	
+	c := time.Tick(time.Duration(interval) * time.Second)
+	for now := range c {
+		println(now.String())
+		go i.SetAllUnits()
+	}
+}
 
-	systemd := dbus.New()
-	units, err := systemd.ListUnits()
+func (i *Init) SetAllUnits() {
+	units, err := i.Systemd.ListUnits()
 	if err != nil {
 		panic(err)
 	}
 
+	prefix := path.Join(keyPrefix, i.Machine.BootId)
 	for _, u := range(units) {
 		if u.ActiveState == "active" {
 			println(u.Name, u.ActiveState)
 			key := path.Join(prefix, u.Name)
-			etcdC.Set(key, "active", 0)
+			i.Etcd.Set(key, "active", 0)
 		}
 	}
+}
+
+func main() {
+	etcdC := etcd.NewClient(nil)
+	mach := machine.NewMachine("")
+	systemd := dbus.New()
+
+	init := Init{etcdC, systemd, mach}
+
+	init.SetAllUnits()
+	init.StartHeartbeat()
 }
