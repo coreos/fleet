5b01fd9897a871c52ce6b24dfb85aec0a2beea99
Brian Waldon <bcwaldon@gmail.com>
2013-11-15 12:54:33 -0800

    feat(main): Allow client to pass in -bootid flag
    The -bootid flag will override the system-level boot ID, which is necessary
    for running multiple copies of coreinit on a single logical server.

--- coreinit.go
+++ coreinit.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"flag"
+
 	"github.com/coreos/coreinit/agent"
 	"github.com/coreos/coreinit/machine"
 	"github.com/coreos/coreinit/registry"
@@ -8,7 +11,17 @@ import (
 )
 
 func main() {
-	m := machine.New(machine.ReadLocalBootId())
+	var bootId string
+
+	f := flag.NewFlagSet(os.Args[0], 1)
+	f.StringVar(&bootId, "bootid", "", "Provide a user-generated boot ID. This will override the actual boot ID of the machine.")
+	f.Parse(os.Args[1:])
+
+	if bootId == "" {
+		bootId = machine.ReadLocalBootId()
+	}
+
+	m := machine.New(bootId)
 	r := registry.New()
 	a := agent.New(r, m, "")
 
