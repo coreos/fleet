ba71cba0c0bf4fbfd331853d96d91d4a7d964b9a
Brian Waldon <bcwaldon@gmail.com>
2013-11-19 15:09:44 -0800

    test(machine): Add more testing of machine pkg

--- machine/machine_test.go
+++ machine/machine_test.go
@@ -16,3 +16,24 @@ func TestNew(t *testing.T) {
 		t.Fatal("machine.Machine.BootId != 'XXX'")
 	}
 }
+
+func TestStringEncoding(t *testing.T) {
+	m1 := Machine{"XXX"}
+	result := m1.String()
+	expect := "XXX"
+	if result != expect {
+		t.Fatalf("machine.Machine.String() returned '%s', expected '%s'", result, expect)
+	}
+}
+
+func TestAddr(t *testing.T) {
+	addr := Addr{"127.0.0.1/32", "ip+net"}
+
+	if addr.Addr != "127.0.0.1/32" {
+		t.Fatal("addr.Addr != '127.0.0.1/32'")
+	}
+
+	if addr.Network != "ip+net" {
+		t.Fatal("addr.Network != 'ip+net'")
+	}
+}
