e4aed4cfbadd022c8883f51dc16f8c2d1eb4bf1a
Brandon Philips <brandon@ifup.co>
2013-11-19 15:57:19 -0800

    feat(cmd): list units lists things, maybe?

--- cmd/list_units.go
+++ cmd/list_units.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
+
+	"github.com/coreos/coreinit/registry"
 )
 
 func listUnits(c *cli.Context) {
+	r := registry.New()
+
+	machines := r.GetActiveMachines()
+
 	println("UNIT\tLOAD\tACTIVE\tSUB\tDESC\tMACHINE")
+	
+	for _, m := range machines {
+		for _, j := range r.GetMachineJobs(&m) {
+			fmt.Printf("%s\t\t\t\t\t%s\n", j.Name, m.String())
+		}
+	}
 }
