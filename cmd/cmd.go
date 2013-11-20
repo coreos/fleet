5632e82555e3282d5fbebcf65d4b44a305ebb2f5
Brandon Philips <brandon@ifup.co>
2013-11-19 22:01:39 -0800

    feat(cmd): add command descriptions.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -11,6 +11,7 @@ var logger *log.Logger
 
 func main() {
 	logger = log.New(os.Stderr, "", 0)
+
 	app := cli.NewApp()
 	app.Name = "corectl"
 	app.Usage = "corectl is a command line driven interface to the cluster wide CoreOS init system."
@@ -20,11 +21,16 @@ func main() {
 		{
 			Name:   "list-units",
 			Usage:  "List installed unit files",
+			Description: `List all of the units that are scheduled on the
+cluster and their current state.`,
 			Action: listUnits,
 		},
 		{
 			Name:   "start",
 			Usage:  "Start (activate) one or more units",
+			Description: `Start adds one or more units to the cluster schedule.
+Once scheduled the cluster will ensure that the unit is
+running on one machine.`,
 			Action: startUnit,
 		},
 	}
