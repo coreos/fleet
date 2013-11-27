ef083cc7847525572c3d0d6219f708ec412a5e1a
Brian Waldon <bcwaldon@gmail.com>
2013-11-27 13:40:05 -0800

    refactor(engine): Breaking apart code in engine pkg for readability

--- engine/engine.go
+++ engine/engine.go
@@ -28,48 +28,152 @@ func NewScheduler(registry *registry.Registry, machine *machine.Machine) *Schedu
 	return &Scheduler{registry, machine, claimTTL}
 }
 
-func (s *Scheduler) DoSchedule() {
+func (scheduler *Scheduler) DoSchedule() {
 	for true {
 		// Let's not be a job-hog
 		time.Sleep(time.Second)
 
-		req := s.Registry.ClaimRequest(s.Machine, s.ClaimTTL)
-		if req == nil {
+		request := scheduler.claimRequest()
+		if request == nil {
 			continue
 		}
 
-		machineMap := s.Registry.GetActiveMachines()
-		machines := make([]machine.Machine, len(machineMap))
-		idx := 0
-		for _, m := range(machineMap) {
-			machines[idx] = m
-			idx++
-		}
+		machines := scheduler.getMachines()
 
-		jobs, err := buildScheduleFromRequest(req, machines)
+		schedule, err := NewScheduleFromJobRequest(request, machines)
 		if err != nil {
-			log.Printf("Unable to resolve request %s: %s", req.ID, err)
+			log.Printf("Unable to resolve job request %s: %s", request.ID, err)
 			continue
 		}
 
-		sched, err := s.completeSchedule(jobs, machines)
+		err = scheduler.finalizeSchedule(&schedule, machines)
 		if err != nil {
-			log.Print(err)
+			log.Printf("Failed to finalize schedule for job request %s: %s", request.ID, err)
 			continue
 		}
 
-		// For now, we assume that if we can initially acquire the lock
-		// we're safe to move forward with scheduling. This is not ideal.
-		for j, m := range sched {
-			s.Registry.ScheduleJob(&j, m)
+		err = scheduler.resolveSchedule(request, &schedule)
+		if err != nil {
+			log.Printf("Failed to resolve schedule for job request %s: %s", request.ID, err)
 		}
-
-		s.Registry.ResolveRequest(req)
 	}
 }
 
-func buildScheduleFromRequest(req *job.JobRequest, machines []machine.Machine) (map[job.Job]*machine.Machine, error) {
-	sched := make(map[job.Job]*machine.Machine, 0)
+func (scheduler *Scheduler) claimRequest() *job.JobRequest {
+	return scheduler.Registry.ClaimRequest(scheduler.Machine, scheduler.ClaimTTL)
+}
+
+func (scheduler *Scheduler) getMachines() []machine.Machine {
+	machineMap := scheduler.Registry.GetActiveMachines()
+	machines := make([]machine.Machine, len(machineMap))
+	idx := 0
+	for _, m := range machineMap {
+		machines[idx] = m
+		idx++
+	}
+
+	return machines
+}
+
+func (scheduler *Scheduler) finalizeSchedule(schedule *Schedule, machines []machine.Machine) error {
+	decide := func(j *job.Job) *machine.Machine {
+		var mach *machine.Machine
+		// If the Job being scheduled is a systemd service unit, we assume we
+		// can put it anywhere. If not, we must find the machine where the
+		// Job's related service file is currently scheduled.
+		if j.Type == "systemd-service" {
+			mach = pickRandomMachine(machines)
+		} else {
+			// This is intended to match a standard filetype (i.e. '.socket' in 'web.socket')
+			re := regexp.MustCompile("\\.(.[a-z]*)$")
+			serviceName := re.ReplaceAllString(j.Name, ".service")
+
+			// Check if the corresponding systemd-service job is referenced in the schedule
+			// we're actively finalize
+			for j2, m := range *schedule {
+				if serviceName == j2.Name {
+					mach = m
+				}
+			}
+
+			if mach == nil {
+				service, _ := job.NewJob(serviceName, nil, nil)
+				if state := scheduler.Registry.GetJobState(service); state != nil {
+					mach = state.Machine
+				}
+			}
+
+			if mach == nil {
+				log.Printf("Unable to schedule job %s since corresponding "+
+					"service job %s could not be found", j.Name, serviceName)
+			}
+		}
+
+		if mach == nil {
+			log.Printf("Not scheduling job %s", j.Name)
+			return nil
+		} else {
+			log.Println("Scheduling job", j.Name, "to machine", mach.BootId)
+			return mach
+		}
+	}
+
+	var undecided []job.Job
+	for j, m := range *schedule {
+		// The schedule may come in partially-completed. We assume any previous
+		// decisions cannot be changed.
+		if m == nil {
+			undecided = append(undecided, j)
+		}
+	}
+
+	// Iterate over the submitted set of undecided jobs up to N+1 times where N=len(jobs).
+	// We assume that N+1 is the theoretical maximum number of attempts that we could possibly
+	// take. This is not proven to be true...
+	iterMax := len(undecided) + 1
+
+	for i := 0; i < iterMax; i++ {
+		decisions := 0
+
+		for i := 0; i < len(undecided); i++ {
+			job := undecided[i-decisions]
+			mach := decide(&job)
+			if mach != nil {
+				(*schedule)[job] = mach
+				undecided = append(undecided[0:i-decisions], undecided[i-decisions+1:]...)
+				decisions++
+			}
+		}
+	}
+
+	if len(undecided) > 0 {
+		return errors.New("Unable to decide how to schedule all jobs")
+	}
+
+	return nil
+}
+
+func pickRandomMachine(machines []machine.Machine) *machine.Machine {
+	target := rand.Intn(len(machines))
+	return &machines[target]
+}
+
+func (scheduler *Scheduler) resolveSchedule(request *job.JobRequest, schedule *Schedule) error {
+	// For now, we assume that if we can initially acquire the lock
+	// we're safe to move forward with scheduling. This is not ideal.
+	for j, m := range *schedule {
+		scheduler.Registry.ScheduleJob(&j, m)
+	}
+
+	scheduler.Registry.ResolveRequest(request)
+
+	return nil
+}
+
+type Schedule map[job.Job]*machine.Machine
+
+func NewScheduleFromJobRequest(req *job.JobRequest, machines []machine.Machine) (Schedule, error) {
+	sched := make(Schedule, 0)
 
 	for i := 0; i < len(req.Payloads); i++ {
 		// Manually create the payload variable so we get a full copy
@@ -103,87 +207,4 @@ func buildScheduleFromRequest(req *job.JobRequest, machines []machine.Machine) (
 		}
 	}
 	return sched, nil
-}
-
-func (s *Scheduler) completeSchedule(sched map[job.Job]*machine.Machine, machines []machine.Machine) (map[job.Job]*machine.Machine, error) {
-	decide := func(j *job.Job) *machine.Machine {
-		var mach *machine.Machine
-		// If the Job being scheduled is a systemd service unit, we assume we
-		// can put it anywhere. If not, we must find the machine where the
-		// Job's related service file is currently scheduled.
-		if j.Type == "systemd-service" {
-			mach = pickRandomMachine(machines)
-		} else {
-			// This is intended to match a standard filetype (i.e. '.socket' in 'web.socket')
-			re := regexp.MustCompile("\\.(.[a-z]*)$")
-			serviceName := re.ReplaceAllString(j.Name, ".service")
-
-			// Check if the corresponding systemd-service job has been scheduled
-			// within this context
-			for j2, m := range sched {
-				if serviceName == j2.Name {
-					mach = m
-				}
-			}
-
-			if mach == nil {
-				service, _ := job.NewJob(serviceName, nil, nil)
-				if state := s.Registry.GetJobState(service); state != nil {
-					mach = state.Machine
-				}
-			}
-
-			if mach == nil {
-				log.Printf("Unable to schedule job %s since corresponding "+
-					"service job %s could not be found", j.Name, serviceName)
-			}
-		}
-
-		if mach == nil {
-			log.Printf("Not scheduling job %s", j.Name)
-			return nil
-		} else {
-			log.Println("Scheduling job", j.Name, "to machine", mach.BootId)
-			return mach
-		}
-	}
-
-	var undecided []job.Job
-	for j, m := range sched {
-		// The schedule may come in partially-completed. We assume any previous
-		// decisions cannot be changed.
-		if m == nil {
-			undecided = append(undecided, j)
-		}
-	}
-
-	// Iterate over the submitted set of undecided jobs up to N+1 times where N=len(jobs).
-	// We assume that N+1 is the theoretical maximum number of attempts that we could possibly
-	// take. This is not proven to be true...
-	iterMax := len(undecided) + 1
-
-	for i := 0; i < iterMax; i++ {
-		decisions := 0
-
-		for i := 0; i < len(undecided); i++ {
-			job := undecided[i-decisions]
-			mach := decide(&job)
-			if mach != nil {
-				sched[job] = mach
-				undecided = append(undecided[0:i-decisions], undecided[i-decisions+1:]...)
-				decisions++
-			}
-		}
-	}
-
-	if len(undecided) > 0 {
-		return nil, errors.New("Unable to decide how to schedule all jobs")
-	}
-
-	return sched, nil
-}
-
-func pickRandomMachine(machines []machine.Machine) *machine.Machine {
-	target := rand.Intn(len(machines))
-	return &machines[target]
 }
