63e68c51a520c12b20f4dcc7a475c1f9850a4e80
Brian Waldon <bcwaldon@gmail.com>
2013-12-04 14:23:09 -0800

    feat(config): define bootid/verbosity in a config file

---
+++ config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"flag"
+	"io"
+	"strconv"
+
+	"github.com/BurntSushi/toml"
+	"github.com/golang/glog"
+
+	"github.com/coreos/coreinit/machine"
+)
+
+type Config struct {
+	BootId    string `toml:"bootid"`
+	Verbosity int    `toml:"verbosity"`
+}
+
+func NewConfig() *Config {
+	bootid := machine.ReadLocalBootId()
+	conf := Config{BootId: bootid, Verbosity: 0}
+	return &conf
+}
+
+func UpdateConfigFromFile(conf *Config, f io.Reader) error {
+	_, err := toml.DecodeReader(f, conf)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateFlagsFromConfig(conf *Config) {
+	err := flag.Lookup("v").Value.Set(strconv.Itoa(conf.Verbosity))
+	if err != nil {
+		glog.Errorf("Failed to apply config.Verbosity to flag.v: %s", err.Error())
+	}
+}
--- coreinit.go
+++ coreinit.go
@@ -2,31 +2,48 @@ package main
 
 import (
 	"flag"
+	"os"
+	"syscall"
 
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/golang/glog"
 
 	"github.com/coreos/coreinit/agent"
+	"github.com/coreos/coreinit/config"
 	"github.com/coreos/coreinit/engine"
 	"github.com/coreos/coreinit/machine"
 	"github.com/coreos/coreinit/registry"
 )
 
 func main() {
-	var bootId string
+	var configPath string
 
-	flag.StringVar(&bootId, "bootid", "", "Provide a user-generated boot ID. This will override the actual boot ID of the machine.")
+	flag.StringVar(&configPath, "config_file", "", "Path to config file.")
 	flag.Parse()
 
-	if bootId == "" {
-		bootId = machine.ReadLocalBootId()
+	conf := config.NewConfig()
+
+	if configPath != "" {
+		configFile, err := os.Open(configPath)
+		if err != nil {
+			glog.Fatalf("Unable to open config file at %s: %s", configPath, err)
+			syscall.Exit(1)
+		}
+
+		err = config.UpdateConfigFromFile(conf, configFile)
+		if err != nil {
+			glog.Fatalf("Failed to parse config file at %s: %s", configPath, err)
+			syscall.Exit(1)
+		}
 	}
+
+	config.UpdateFlagsFromConfig(conf)
 
 	if glog.V(2) {
 		etcd.OpenDebug()
 	}
 
-	m := machine.New(bootId)
+	m := machine.New(conf.BootId)
 	r := registry.New()
 	es := registry.NewEventStream()
 
