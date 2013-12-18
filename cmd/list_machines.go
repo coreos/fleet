9777c0e8d8007f21bff4f405b6d4283514045f5b
Brian Waldon <bcwaldon@gmail.com>
2013-12-18 14:43:59 -0800

    feat(corectl): Add list-machines command

---
+++ cmd/list_machines.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/codegangsta/cli"
+)
+
+func newListMachinesCommand() cli.Command {
+	return cli.Command{
+		Name:  "list-machines",
+		Usage: "List all machines.",
+		Description: "",
+		Action: listMachinesAction,
+	}
+}
+
+func listMachinesAction(c *cli.Context) {
+	r := getRegistry(c)
+
+	fmt.Fprintln(out, "MACHINE")
+
+	for _, m := range r.GetActiveMachines() {
+		fmt.Fprintf(out, "%s\n", m.BootId)
+	}
+
+	out.Flush()
+}
