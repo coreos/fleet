5c179951ea11b95c5c5c1e74da413192450db5f8
Brian Waldon <bcwaldon@gmail.com>
2014-01-24 16:10:09 -0800

    bump(github.com/coreos/go-systemd): 4a3b33ed414ad42fddee2a4848c4cb9ad36a0677

--- third_party/src/github.com/coreos/go-systemd/activation/files.go
+++ third_party/src/github.com/coreos/go-systemd/activation/files.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2013 CoreOS Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // Package activation implements primitives for systemd socket activation.
 package activation
 
@@ -13,7 +29,6 @@ const (
 )
 
 func Files(unsetEnv bool) []*os.File {
-
 	if unsetEnv {
 		// there is no way to unset env in golang os package for now
 		// https://code.google.com/p/go/issues/detail?id=6423
@@ -25,14 +40,17 @@ func Files(unsetEnv bool) []*os.File {
 	if err != nil || pid != os.Getpid() {
 		return nil
 	}
+
 	nfds, err := strconv.Atoi(os.Getenv("LISTEN_FDS"))
 	if err != nil || nfds == 0 {
 		return nil
 	}
+
 	var files []*os.File
 	for fd := listenFdsStart; fd < listenFdsStart+nfds; fd++ {
 		syscall.CloseOnExec(fd)
 		files = append(files, os.NewFile(uintptr(fd), "LISTEN_FD_"+strconv.Itoa(fd)))
 	}
+
 	return files
 }
