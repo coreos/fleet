bf47f4386f22b300abe69b1347512716d1542746
Brian Waldon <bcwaldon@gmail.com>
2014-01-26 14:56:15 -0800

    refactor(engine): Move capability code from event.go to engine.go

--- engine/engine.go
+++ engine/engine.go
@@ -1,11 +1,13 @@
 package engine
 
 import (
+	"errors"
 	"time"
 
 	log "github.com/golang/glog"
 
 	"github.com/coreos/coreinit/event"
+	"github.com/coreos/coreinit/job"
 	"github.com/coreos/coreinit/machine"
 	"github.com/coreos/coreinit/registry"
 )
@@ -20,7 +22,7 @@ type Engine struct {
 	machine  *machine.Machine
 	claimTTL time.Duration
 
-	stop     chan bool
+	stop chan bool
 }
 
 func New(reg *registry.Registry, events *event.EventBus, mach *machine.Machine) *Engine {
@@ -45,14 +47,100 @@ func (self *Engine) Stop() {
 	close(self.stop)
 }
 
-func (self *Engine) Registry() *registry.Registry {
-	return self.registry
+func (self *Engine) GetJobsScheduledToMachine(machName string) []job.Job {
+	var jobs []job.Job
+
+	for _, j := range self.registry.GetAllJobs() {
+		tgt := self.registry.GetJobTarget(j.Name)
+		if tgt == nil || tgt.BootId != machName {
+			continue
+		}
+		jobs = append(jobs, j)
+	}
+
+	return jobs
 }
 
-func (self *Engine) Machine() *machine.Machine {
-	return self.machine
+func (self *Engine) CancelJob(jobName string) {
+	self.registry.CancelJob(jobName)
+	log.Info("Cancelled Job(%s)", jobName)
 }
 
-func (self *Engine) ClaimTTL() time.Duration {
-	return self.claimTTL
+func (self *Engine) ResolveRequest(request *job.JobRequest) error {
+	log.V(2).Infof("Attempting to claim JobRequest(%s)", request.ID.String())
+	if !self.claimRequest(request) {
+		log.V(2).Infof("Could not claim JobRequest(%s)", request.ID.String())
+		return errors.New("Could not claim JobRequest")
+	}
+
+	log.V(1).Infof("Claimed JobRequest(%s)", request.ID.String())
+
+	for _, j := range getJobsFromRequest(request) {
+		log.V(2).Infof("Creating Job(%s) from JobRequest(%s)", j.Name, request.ID.String())
+		self.registry.CreateJob(&j)
+		log.Infof("Created Job(%s) from JobRequest(%s)", j.Name, request.ID.String())
+	}
+
+	log.V(2).Infof("Resolving JobRequest(%s)", request.ID.String())
+	self.registry.ResolveRequest(request)
+	log.V(1).Infof("Resolved JobRequest(%s)", request.ID.String())
+
+	return nil
+}
+
+func getJobsFromRequest(req *job.JobRequest) []job.Job {
+	var jobs []job.Job
+	for i := 0; i < len(req.Payloads); i++ {
+		payload := req.Payloads[i]
+		j := job.NewJob(payload.Name, nil, &payload)
+		jobs = append(jobs, *j)
+	}
+	return jobs
+}
+
+func (self *Engine) OfferJob(j job.Job) error {
+	log.V(2).Infof("Attempting to claim Job(%s)", j.Name)
+	if !self.claimJob(j.Name) {
+		log.V(1).Infof("Could not claim Job(%s)", j.Name)
+		return errors.New("Could not claim Job")
+	}
+
+	log.V(1).Infof("Claimed Job", j.Name)
+
+	offer := job.NewOfferFromJob(j)
+
+	log.V(2).Infof("Publishing JobOffer(%s)", offer.Job.Name)
+	self.registry.CreateJobOffer(offer)
+	log.Infof("Published JobOffer(%s)", offer.Job.Name)
+
+	return nil
+}
+
+func (self *Engine) ResolveJobOffer(jobName string, machName string) error {
+	log.V(2).Infof("Attempting to claim JobOffer(%s)", jobName)
+	if !self.claimJobOffer(jobName) {
+		log.V(2).Infof("Could not claim JobOffer(%s)", jobName)
+		return errors.New("Could not claim JobOffer")
+	}
+
+	log.V(2).Infof("Claimed JobOffer", jobName)
+
+	log.V(2).Infof("Resolving JobOffer(%s), scheduling to Machine(%s)", jobName, machName)
+	self.registry.ResolveJobOffer(jobName)
+	self.registry.ScheduleJob(jobName, machName)
+	log.Infof("Scheduled Job(%s) to Machine(%s)", jobName, machName)
+
+	return nil
+}
+
+func (self *Engine) claimJobOffer(jobName string) bool {
+	return self.registry.ClaimJobOffer(jobName, self.machine, self.claimTTL)
+}
+
+func (self *Engine) claimJob(jobName string) bool {
+	return self.registry.ClaimJob(jobName, self.machine, self.claimTTL)
+}
+
+func (self *Engine) claimRequest(req *job.JobRequest) bool {
+	return self.registry.ClaimRequest(req, self.machine, self.claimTTL)
 }
--- engine/event.go
+++ engine/event.go
@@ -18,95 +18,45 @@ func NewEventHandler(engine *Engine) *EventHandler {
 func (self *EventHandler) HandleEventRequestCreated(ev event.Event) {
 	request := ev.Payload.(job.JobRequest)
 
-	log.V(1).Infof("EventRequestCreated(%s): attempting to claim JobRequest", request.ID.String())
-	if !self.claimRequest(&request) {
-		log.V(1).Infof("EventRequestCreated(%s): could not claim JobRequest", request.ID.String())
-		return
+	log.V(1).Infof("EventRequestCreated(%s): attempting to resolve JobRequest", request.ID.String())
+	err := self.engine.ResolveRequest(&request)
+	if err == nil {
+		log.V(1).Infof("EventRequestCreated(%s): resolved JobRequest", request.ID.String())
 	} else {
-		log.V(1).Infof("EventRequestCreated(%s): claimed JobRequest", request.ID.String())
+		log.V(1).Infof("EventRequestCreated(%s): failed to resolve JobRequest: %s", request.ID.String(), err.Error())
 	}
-
-	for _, j := range getJobsFromRequest(&request) {
-		log.Infof("EventRequestCreated(%s): creating Job(%s)", request.ID.String(), j.Name)
-		self.engine.Registry().CreateJob(&j)
-	}
-
-	log.Infof("EventRequestCreated(%s): resolving JobRequest", request.ID.String())
-	self.engine.Registry().ResolveRequest(&request)
-}
-
-func getJobsFromRequest(req *job.JobRequest) []job.Job {
-	var jobs []job.Job
-	for i := 0; i < len(req.Payloads); i++ {
-		payload := req.Payloads[i]
-		j := job.NewJob(payload.Name, nil, &payload)
-		jobs = append(jobs, *j)
-	}
-	return jobs
-}
-
-func (self *EventHandler) claimRequest(req *job.JobRequest) bool {
-	return self.engine.Registry().ClaimRequest(req, self.engine.Machine(), self.engine.ClaimTTL())
 }
 
 func (self *EventHandler) HandleEventJobCreated(ev event.Event) {
 	j := ev.Payload.(job.Job)
-	log.V(1).Infof("EventJobCreated(%s): Job=%s", j.Name, j.String())
 
-	log.V(1).Infof("EventJobCreated(%s): attempting to claim Job", j.Name)
-	if !self.claimJob(j.Name) {
-		log.V(1).Infof("EventJobCreated(%s): unable to claim Job", j.Name)
-		return
-	} else {
-		log.V(1).Infof("EventJobCreated(%s): claimed Job", j.Name)
-	}
-
-	offer := job.NewOfferFromJob(j)
-	log.V(1).Infof("EventJobCreated(%s): created JobOffer(%s)", j.Name, offer.Job.Name)
-
-	log.Infof("EventJobCreated(%s): publishing JobOffer(%s)", j.Name, offer.Job.Name)
-	self.engine.Registry().CreateJobOffer(offer)
-}
-
-func (self *EventHandler) claimJob(jobName string) bool {
-	return self.engine.Registry().ClaimJob(jobName, self.engine.Machine(), self.engine.ClaimTTL())
+	log.V(1).Infof("EventJobCreated(%s): publishing JobOffer", j.Name)
+	self.engine.OfferJob(j)
 }
 
 func (self *EventHandler) HandleEventJobBidSubmitted(ev event.Event) {
 	jb := ev.Payload.(job.JobBid)
 
-	log.V(1).Infof("EventJobBidSubmitted(%s): attempting to claim JobOffer", jb.JobName)
-	if !self.claimJobOffer(jb.JobName) {
-		log.V(1).Infof("EventJobBidSubmitted(%s): could not claim JobOffer", jb.JobName)
-		return
+	log.V(1).Infof("EventJobBidSubmitted(%s): attempting to schedule Job to Machine(%s)", jb.JobName, jb.MachineName)
+	err := self.engine.ResolveJobOffer(jb.JobName, jb.MachineName)
+	if err == nil {
+		log.V(1).Infof("EventJobBidSubmitted(%s): successfully scheduled Job to Machine(%s)", jb.JobName, jb.MachineName)
 	} else {
-		log.V(1).Infof("EventJobBidSubmitted(%s): claimed JobOffer", jb.JobName)
+		log.V(1).Infof("EventJobBidSubmitted(%s): failed to schedule Job to Machine(%s): %s", jb.JobName, jb.MachineName, err.Error())
 	}
-
-	log.V(1).Infof("EventJobBidSubmitted(%s): accepted JobBid from Machine(%s), resolving JobOffer", jb.JobName, jb.MachineName)
-	self.engine.Registry().ResolveJobOffer(jb.JobName)
-
-	log.Infof("EventJobBidSubmitted(%s): instructing Machine(%s) to run Job", jb.JobName, jb.MachineName)
-	self.engine.Registry().ScheduleJob(jb.JobName, jb.MachineName)
-}
-
-func (self *EventHandler) claimJobOffer(jobName string) bool {
-	return self.engine.Registry().ClaimJobOffer(jobName, self.engine.Machine(), self.engine.ClaimTTL())
 }
 
 func (self *EventHandler) HandleEventMachineRemoved(ev event.Event) {
 	machName := ev.Payload.(string)
-	for _, j := range self.engine.Registry().GetAllJobs() {
-		tgt := self.engine.Registry().GetJobTarget(j.Name)
-		if tgt == nil || tgt.BootId != machName {
-			continue
-		}
+	jobs := self.engine.GetJobsScheduledToMachine(machName)
 
+	for _, j := range jobs {
 		log.V(1).Infof("EventMachineRemoved(%s): cancelling Job(%s)", machName, j.Name)
-		self.engine.Registry().CancelJob(j.Name)
+		self.engine.CancelJob(j.Name)
+	}
 
-		offer := job.NewOfferFromJob(j)
-		log.V(1).Infof("EventMachineRemoved(%s): re-publishing JobOffer(%s)", machName, offer.Job.Name)
-		self.engine.Registry().CreateJobOffer(offer)
+	for _, j := range jobs {
+		log.V(1).Infof("EventMachineRemoved(%s): re-publishing JobOffer(%s)", machName, j.Name)
+		self.engine.OfferJob(j)
 	}
 }
