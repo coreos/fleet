5b9259c230aef09fb7a969082c72eb3e16316edc
Brian Waldon <bcwaldon@gmail.com>
2014-02-19 19:30:17 -0800

    test(machine): add testing of Machine.HasMetadata

--- machine/machine_test.go
+++ machine/machine_test.go
@@ -39,3 +39,61 @@ func TestStackStateEmptyTop(t *testing.T) {
 		t.Errorf("Unexpected Metadata %v", stacked.Metadata)
 	}
 }
+
+func TestHasMetadataSimpleMatch(t *testing.T) {
+	metadata := map[string]string{
+		"region": "us-east-1",
+	}
+	mach := New("", "", metadata)
+
+	match := map[string][]string{
+		"region": []string{"us-east-1"},
+	}
+	if !mach.HasMetadata(match) {
+		t.Errorf("Machine reported it did not have expected state")
+	}
+}
+
+func TestHasMetadataMultiMatch(t *testing.T) {
+	metadata := map[string]string{
+		"groups": "ping",
+	}
+	mach := New("", "", metadata)
+
+	match := map[string][]string{
+		"groups": []string{"ping", "pong"},
+	}
+	if !mach.HasMetadata(match) {
+		t.Errorf("Machine reported it did not have expected state")
+	}
+}
+
+func TestHasMetadataSingleMatchFail(t *testing.T) {
+	metadata := map[string]string{
+		"groups": "ping",
+	}
+	mach := New("", "", metadata)
+
+	match := map[string][]string{
+		"groups": []string{"pong"},
+	}
+	if mach.HasMetadata(match) {
+		t.Errorf("Machine reported a successful match for metadata which it does not have")
+	}
+}
+
+func TestHasMetadataPartialMatchFail(t *testing.T) {
+	metadata := map[string]string{
+		"region": "us-east-1",
+		"groups": "ping",
+	}
+	mach := New("", "", metadata)
+
+	match := map[string][]string{
+		"region": []string{"us-east-1"},
+		"groups": []string{"pong"},
+	}
+	if mach.HasMetadata(match) {
+		t.Errorf("Machine reported a successful match for metadata which it does not have")
+	}
+}
