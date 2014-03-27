ac71143516c7ddddb78ca282d955b8d718c8bce2
Brian Waldon <bcwaldon@gmail.com>
2014-03-26 20:25:47 -0700

    refactor(systemd): Store raw unit files

--- unit/file.go
+++ unit/file.go
@@ -6,21 +6,38 @@ import (
 )
 
 type SystemdUnitFile struct {
-	Contents map[string]map[string]string
+	// Contents represents the parsed unit file.
+	// This field must be considered readonly.
+	Contents map[string]map[string][]string
+
+	raw      string
 }
 
-func (self *SystemdUnitFile) GetSection(section string) map[string]string {
-	result, ok := self.Contents[section]
-	if ok {
-		return result
-	} else {
-		return make(map[string]string, 0)
+func (self *SystemdUnitFile) String() string {
+	return self.raw
+}
+
+// LegacyContents serializes the contents of a unit file into an obsolete datastructure. This
+// datastructure is lossy and should only be used to remain backwards-compatible where necessary.
+func (self *SystemdUnitFile) LegacyContents() map[string]map[string]string {
+	coerced := make(map[string]map[string]string, len(self.Contents))
+	for section, options := range self.Contents {
+		coerced[section] = make(map[string]string)
+		for key, values := range options {
+			if len(values) == 0 {
+				continue
+			}
+			coerced[section][key] = values[len(values)-1]
+		}
 	}
+	return coerced
 }
 
+// Requirements returns all relevant options from the [X-Fleet] section
+// of a unit file. Relevant options are identified with a `X-` prefix.
 func (self *SystemdUnitFile) Requirements() map[string][]string {
-	requirements := make(map[string][]string, 0)
-	for key, value := range self.GetSection("X-Fleet") {
+	requirements := make(map[string][]string)
+	for key, value := range self.Contents["X-Fleet"] {
 		if !strings.HasPrefix(key, "X-") {
 			continue
 		}
@@ -32,37 +49,45 @@ func (self *SystemdUnitFile) Requirements() map[string][]string {
 			requirements[key] = make([]string, 0)
 		}
 
-		requirements[key] = append(requirements[key], value)
+		requirements[key] = value
 	}
+
 	return requirements
 }
 
+// Description returns the first Description option found in the [Unit] section.
+// If the option is not defined, an empty string is returned.
 func (self *SystemdUnitFile) Description() string {
-	return self.GetSection("Unit")["Description"]
+	if values := self.Contents["Unit"]["Description"]; len(values) > 0 {
+		return values[0]
+	}
+	return ""
 }
 
-func (self *SystemdUnitFile) String() string {
+func NewSystemdUnitFile(raw string) *SystemdUnitFile {
+	parsed := deserializeUnitFile(raw)
+	return &SystemdUnitFile{parsed, raw}
+}
+
+// NewSystemdUnitFileFromLegacyContents creates a SystemdUnitFile object from an obsolete unit
+// file datastructure. This should only be used to remain backwards-compatible where necessary.
+func NewSystemdUnitFileFromLegacyContents(contents map[string]map[string]string) *SystemdUnitFile {
 	var serialized string
-	for section, keyMap := range self.Contents {
+	for section, keyMap := range contents {
 		serialized += fmt.Sprintf("[%s]\n", section)
 		for key, value := range keyMap {
 			serialized += fmt.Sprintf("%s=%s\n", key, value)
 		}
 		serialized += "\n"
 	}
-	return serialized
+	return NewSystemdUnitFile(serialized)
 }
 
-func NewSystemdUnitFile(Contents string) *SystemdUnitFile {
-	parsed := deserializeUnitFile(Contents)
-	return &SystemdUnitFile{parsed}
-}
-
-// This is dangerously simple and should be rewritten to match the spec
-func deserializeUnitFile(contents string) map[string]map[string]string {
-	sections := make(map[string]map[string]string, 0)
+// deserializeUnitFile is dangerously simple and should be rewritten to match the systemd unit file spec
+func deserializeUnitFile(raw string) map[string]map[string][]string {
+	sections := make(map[string]map[string][]string)
 	var section string
-	for _, line := range strings.Split(contents, "\n") {
+	for _, line := range strings.Split(raw, "\n") {
 		// Ignore commented-out lines
 		if strings.HasPrefix(line, "#") || strings.HasPrefix(line, ";") {
 			continue
@@ -77,8 +102,8 @@ func deserializeUnitFile(contents string) map[string]map[string]string {
 
 		// Check for section
 		if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
-			section = line[1:len(line)-1]
-			sections[section] = make(map[string]string, 0)
+			section = line[1 : len(line)-1]
+			sections[section] = make(map[string][]string)
 			continue
 		}
 
@@ -89,7 +114,7 @@ func deserializeUnitFile(contents string) map[string]map[string]string {
 			value := strings.Trim(parts[1], " ")
 
 			if len(section) > 0 {
-				sections[section][key] = value
+				sections[section][key] = append(sections[section][key], value)
 			}
 
 		}
