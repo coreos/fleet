efe183634c5a13b03a8293e186b94caeaaf81eb2
Brian Waldon <bcwaldon@gmail.com>
2014-03-31 16:51:23 -0700

    test(functional): Robustify functional testing

--- functional/smoke_test.go
+++ functional/smoke_test.go
@@ -41,7 +41,7 @@ func fleetctl(args ...string) (string, string, error) {
 	return stdoutBytes.String(), stderrBytes.String(), err
 }
 
-func TestMachineList(t *testing.T) {
+func TestCluster(t *testing.T) {
 	cluster, err := platform.NewNspawnCluster("smoke")
 	if err != nil {
 		t.Fatalf(err.Error())
@@ -52,55 +52,39 @@ func TestMachineList(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 
-	stdout, _, err := fleetctl("list-machines", "--no-legend")
-	if err != nil {
-		t.Fatalf("Failed to run list-machines: %v", err)
+	var machines []string
+	for i := 0; i <= 7; i++ {
+		if i == 7 {
+			t.Fatalf("Failed to find all three machines within the time limit")
+		}
+
+		log.Printf("Waiting 5s for fleet services to check in...")
+		time.Sleep(5*time.Second)
+
+		stdout, _, err := fleetctl("list-machines", "--no-legend", "-l")
+		if err != nil {
+			t.Fatalf("Failed to run list-machines: %v", err)
+		}
+
+		stdout = strings.TrimSpace(stdout)
+		machines = strings.Split(stdout, "\n")
+		if len(machines) == 3 {
+			for k, v := range machines {
+				machines[k] = strings.SplitN(v, "\t", 2)[0]
+			}
+			break
+		}
 	}
 
-	stdout = strings.TrimSpace(stdout)
-	machines := strings.Split(stdout, "\n")
-	if len(machines) != 3 {
-		t.Errorf("Did not find three machines running: \n%s", stdout)
-	}
-}
-
-func TestMachineSSH(t *testing.T) {
-	cluster, err := platform.NewNspawnCluster("smoke")
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	defer cluster.DestroyAll()
-
-	if err := cluster.Create(3); err != nil {
-		t.Fatalf(err.Error())
-	}
-
-	stdout, _, err := fleetctl("list-machines", "--no-legend", "-l")
-	if err != nil {
-		t.Fatalf("Failed to run list-machines: %v", err)
-	}
-
-	bootID := strings.SplitN(stdout, "\t", 2)[0]
-	stdout, _, err = fleetctl("ssh", bootID, "uptime")
-	if err != nil {
-		t.Fatalf("Unable to SSH into fleet machine: %v", err)
-	}
-}
-
-func TestClusterGrowth(t *testing.T) {
-	cluster, err := platform.NewNspawnCluster("smoke")
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	defer cluster.DestroyAll()
-
-	if err := cluster.Create(3); err != nil {
-		t.Fatalf(err.Error())
+	for _, machine := range machines {
+		if _, _, err := fleetctl("ssh", "--strict-host-key-checking=false", machine, "uptime"); err != nil {
+			t.Errorf("Unable to SSH into fleet machine: %v", err)
+		}
 	}
 
 	for i := 0; i < 5; i++ {
 		unitName := fmt.Sprintf("fixtures/units/conflict.%d.service", i)
-		_, _, err := fleetctl("start", unitName)
+		_, _, err := fleetctl("start", "--no-block",  unitName)
 		if err != nil {
 			t.Fatalf("Failed to start unit %s: %v", unitName, err)
 		}
@@ -144,14 +128,27 @@ func TestClusterGrowth(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 
-	stdout, _, err = fleetctl("list-machines", "--no-legend")
-	if err != nil {
-		t.Fatalf("Failed to run list-machines: %v", err)
-	}
+	for i := 0; i <= 7; i++ {
+		if i == 7 {
+			t.Fatalf("Failed to find all three machines within the time limit")
+		}
 
-	machines := strings.Split(strings.TrimSpace(stdout), "\n")
-	if len(machines) != 5 {
-		t.Fatalf("Did not find five machines running: \n%s", stdout)
+		log.Printf("Waiting 5s for fleet services to check in...")
+		time.Sleep(5*time.Second)
+
+		stdout, _, err := fleetctl("list-machines", "--no-legend", "-l")
+		if err != nil {
+			t.Fatalf("Failed to run list-machines: %v", err)
+		}
+
+		stdout = strings.TrimSpace(stdout)
+		machines = strings.Split(stdout, "\n")
+		if len(machines) == 5 {
+			for k, v := range machines {
+				machines[k] = strings.SplitN(v, "\t", 2)[0]
+			}
+			break
+		}
 	}
 
 	for i := 0; i < 5; i++ {
