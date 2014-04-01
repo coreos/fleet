3816e778292e66f1266c246fb1ecb55020021a07
Brian Waldon <bcwaldon@gmail.com>
2014-03-31 17:22:25 -0700

    test(functional): factor out helper functions

--- functional/smoke_test.go
+++ functional/smoke_test.go
@@ -1,9 +1,9 @@
 package functional
 
 import (
-	"log"
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
@@ -31,16 +31,6 @@ func init() {
 	}
 }
 
-func fleetctl(args ...string) (string, string, error) {
-	log.Printf("%s %s", fleetctlBinPath, strings.Join(args, " "))
-	var stdoutBytes, stderrBytes bytes.Buffer
-	cmd := exec.Command(fleetctlBinPath, args...)
-	cmd.Stdout = &stdoutBytes
-	cmd.Stderr = &stderrBytes
-	err := cmd.Run()
-	return stdoutBytes.String(), stderrBytes.String(), err
-}
-
 func TestCluster(t *testing.T) {
 	cluster, err := platform.NewNspawnCluster("smoke")
 	if err != nil {
@@ -48,131 +38,56 @@ func TestCluster(t *testing.T) {
 	}
 	defer cluster.DestroyAll()
 
+	// Start with a simple three-node cluster
 	if err := cluster.Create(3); err != nil {
 		t.Fatalf(err.Error())
 	}
-
-	var machines []string
-	for i := 0; i <= 7; i++ {
-		if i == 7 {
-			t.Fatalf("Failed to find all three machines within the time limit")
-		}
-
-		log.Printf("Waiting 5s for fleet services to check in...")
-		time.Sleep(5*time.Second)
-
-		stdout, _, err := fleetctl("list-machines", "--no-legend", "-l")
-		if err != nil {
-			t.Fatalf("Failed to run list-machines: %v", err)
-		}
-
-		stdout = strings.TrimSpace(stdout)
-		machines = strings.Split(stdout, "\n")
-		if len(machines) == 3 {
-			for k, v := range machines {
-				machines[k] = strings.SplitN(v, "\t", 2)[0]
-			}
-			break
-		}
+	machines, err := waitForNMachines(3)
+	if err != nil {
+		t.Fatalf(err.Error())
 	}
 
+	// Ensure we can SSH into each machine using fleetctl
 	for _, machine := range machines {
 		if _, _, err := fleetctl("ssh", "--strict-host-key-checking=false", machine, "uptime"); err != nil {
 			t.Errorf("Unable to SSH into fleet machine: %v", err)
 		}
 	}
 
+	// Start the 5 services
 	for i := 0; i < 5; i++ {
 		unitName := fmt.Sprintf("fixtures/units/conflict.%d.service", i)
-		_, _, err := fleetctl("start", "--no-block",  unitName)
+		_, _, err := fleetctl("start", "--no-block", unitName)
 		if err != nil {
-			t.Fatalf("Failed to start unit %s: %v", unitName, err)
+			t.Errorf("Failed starting %s: %v", unitName, err)
 		}
 	}
 
+	// All 5 services should be visible immediately and become ACTIVE
+	// shortly thereafter
 	stdout, _, err := fleetctl("list-units", "--no-legend")
 	if err != nil {
 		t.Fatalf("Failed to run list-units: %v", err)
 	}
-
 	units := strings.Split(strings.TrimSpace(stdout), "\n")
 	if len(units) != 5 {
 		t.Fatalf("Did not find five units in cluster: \n%s", stdout)
 	}
-
-	for i := 0; i < 5; i++ {
-		stdout, _, err := fleetctl("list-units", "--no-legend")
-		if err != nil {
-			t.Fatalf("Failed to run list-units: %v", err)
-		}
-
-		units := strings.Split(strings.TrimSpace(stdout), "\n")
-		if len(units) != 5 {
-			t.Fatalf("Did not find five units in cluster: \n%s", stdout)
-		}
-
-		states := parseUnitStates(units)
-		if activeCount(states) != 3 {
-			if i == 4 {
-				t.Fatalf("Three units did not become active in time")
-			}
-
-			time.Sleep(time.Second)
-			continue
-		}
-
-		break
-	}
-
-	if err := cluster.Create(2); err != nil {
+	if err := waitForNActiveUnits(3); err != nil {
 		t.Fatalf(err.Error())
 	}
 
-	for i := 0; i <= 7; i++ {
-		if i == 7 {
-			t.Fatalf("Failed to find all three machines within the time limit")
-		}
-
-		log.Printf("Waiting 5s for fleet services to check in...")
-		time.Sleep(5*time.Second)
-
-		stdout, _, err := fleetctl("list-machines", "--no-legend", "-l")
-		if err != nil {
-			t.Fatalf("Failed to run list-machines: %v", err)
-		}
-
-		stdout = strings.TrimSpace(stdout)
-		machines = strings.Split(stdout, "\n")
-		if len(machines) == 5 {
-			for k, v := range machines {
-				machines[k] = strings.SplitN(v, "\t", 2)[0]
-			}
-			break
-		}
+	// Add two more machines to the cluster and ensure the remaining
+	// unscheduled services are picked up.
+	if err := cluster.Create(2); err != nil {
+		t.Fatalf(err.Error())
 	}
-
-	for i := 0; i < 5; i++ {
-		stdout, _, err := fleetctl("list-units", "--no-legend")
-		if err != nil {
-			t.Fatalf("Failed to run list-units: %v", err)
-		}
-
-		units := strings.Split(strings.TrimSpace(stdout), "\n")
-		if len(units) != 5 {
-			t.Fatalf("Did not find five units in cluster: \n%s", stdout)
-		}
-
-		states := parseUnitStates(units)
-		if activeCount(states) != 5 {
-			if i == 4 {
-				t.Fatalf("Five units did not become active in time")
-			}
-
-			time.Sleep(time.Second)
-			continue
-		}
-
-		break
+	machines, err = waitForNMachines(5)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if err := waitForNActiveUnits(5); err != nil {
+		t.Fatalf(err.Error())
 	}
 }
 
@@ -194,4 +109,70 @@ func activeCount(states []string) (count int) {
 		}
 	}
 	return
+}
+
+func fleetctl(args ...string) (string, string, error) {
+	log.Printf("%s %s", fleetctlBinPath, strings.Join(args, " "))
+	var stdoutBytes, stderrBytes bytes.Buffer
+	cmd := exec.Command(fleetctlBinPath, args...)
+	cmd.Stdout = &stdoutBytes
+	cmd.Stderr = &stderrBytes
+	err := cmd.Run()
+	return stdoutBytes.String(), stderrBytes.String(), err
+}
+
+func waitForNMachines(count int) ([]string, error) {
+	var machines []string
+	for i := 0; i <= 7; i++ {
+		if i == 7 {
+			return nil, fmt.Errorf("Failed to find %d machines within the time limit", count)
+		}
+
+		log.Printf("Waiting 5s for %d fleet services to check in...", count)
+		time.Sleep(5 * time.Second)
+
+		stdout, _, err := fleetctl("list-machines", "--no-legend", "-l")
+		if err != nil {
+			continue
+		}
+
+		machines := strings.Split(strings.TrimSpace(stdout), "\n")
+		if len(machines) != count {
+			continue
+		}
+
+		for k, v := range machines {
+			machines[k] = strings.SplitN(v, "\t", 2)[0]
+		}
+
+		break
+	}
+
+	return machines, nil
+}
+
+func waitForNActiveUnits(count int) error {
+	for i := 0; i <= 6; i++ {
+		if i == 6 {
+			return fmt.Errorf("Failed to find %d active units within the time limit", count)
+		}
+
+		log.Printf("Waiting 1s for %d fleet units to become active...", count)
+		time.Sleep(time.Second)
+
+		stdout, _, err := fleetctl("list-units", "--no-legend")
+		if err != nil {
+			continue
+		}
+
+		units := strings.Split(strings.TrimSpace(stdout), "\n")
+		states := parseUnitStates(units)
+		if activeCount(states) != count {
+			continue
+		}
+
+		break
+	}
+
+	return nil
 }
