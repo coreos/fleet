b6cc7de86e3baa1fe071bbe0a99c5b857a6f9b73
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2014-04-02 18:06:05 -0700

    feat(test): test code format

--- test.sh
+++ test.sh
@@ -3,10 +3,20 @@
 source ./build
 
 if [ -z "$PKG" ]; then
-    PKG="./agent ./config ./event ./fleetctl ./job ./machine ./pkg ./registry ./sign ./ssh ./unit ./integration-tests"
+	PKG="./agent ./config ./event ./fleetctl ./job ./machine ./pkg ./registry ./sign ./ssh ./unit ./integration-tests"
+	GOFMTPATH="$PKG ./engine ./functional ./server ./systemd fleet.go"
+else
+	GOFMTPATH="$PKG"
 fi
 
 # Unit tests
 echo
 go test -i $PKG
 go test -cover -v $PKG
+
+fmtRes=`gofmt -l $GOFMTPATH`
+if [ "$fmtRes" != "" ]; then
+	echo "Failed to pass golang format checking:"
+	echo $fmtRes
+	exit 1
+fi
