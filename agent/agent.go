14222ba98e6b0331a2052e214b5d4c4a236ac49e
Brian Waldon <bcwaldon@gmail.com>
2014-04-04 15:47:49 -0700

    fix(agent): Simplify purging jobs
    The Agent.StopJob function is intended to be triggered by users and has
    a very destructive impact on the cluster. When calling Agent.Purge, the
    churn created by StopJob causes jobs that need to be rescheduled to
    completely stop. Manually purging state and ignoring peers allows jobs
    to reliably get rescheduled.

--- agent/agent.go
+++ agent/agent.go
@@ -126,7 +126,10 @@ func (a *Agent) Purge() {
 	}
 
 	for _, j := range a.registry.GetAllJobsByMachine(bootID) {
-		a.StopJob(j.Name)
+		log.Infof("Purging Job(%s)", j.Name)
+		a.systemd.StopJob(j.Name)
+		a.ForgetJob(j.Name)
+		a.ReportJobState(j.Name, nil)
 
 		a.VerifyJob(&j)
 
