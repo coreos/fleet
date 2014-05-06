9b591f9df858c48acdedc56ac16d5f17313ee055
Brian Waldon <bcwaldon@gmail.com>
2014-05-05 20:27:50 -0700

    refactor(systemd): Drop dead code

--- systemd/unit.go
+++
@@ -1,9 +0,0 @@
-package systemd
-
-type SystemdUnit interface {
-	Name() string
-
-	// The first three strings correspond to the LoadState,
-	// ActiveState and SubState of a unit.
-	State() (string, string, string, []string, error)
-}
