8d48e3cddf6625ed855ac42d5e82209500c9cb8e
Brian Waldon <bcwaldon@gmail.com>
2014-05-16 19:21:39 -0700

    refactor(TestRegistry): panic in unimpl methods; use ptr receivers

--- registry/fake.go
+++ registry/fake.go
@@ -23,15 +23,15 @@ type TestRegistry struct {
 	version   *semver.Version
 }
 
-func (t TestRegistry) GetActiveMachines() ([]machine.MachineState, error) {
+func (t *TestRegistry) GetActiveMachines() ([]machine.MachineState, error) {
 	return t.machines, nil
 }
 
-func (t TestRegistry) GetAllJobs() ([]job.Job, error) {
+func (t *TestRegistry) GetAllJobs() ([]job.Job, error) {
 	return t.jobs, nil
 }
 
-func (t TestRegistry) GetJob(name string) (*job.Job, error) {
+func (t *TestRegistry) GetJob(name string) (*job.Job, error) {
 	for _, j := range t.jobs {
 		if j.Name == name {
 			j.UnitState = t.jobStates[name]
@@ -41,27 +41,27 @@ func (t TestRegistry) GetJob(name string) (*job.Job, error) {
 	return nil, nil
 }
 
-func (t TestRegistry) SetJobTargetState(name string, target job.JobState) error {
-	return nil
+func (t *TestRegistry) SetJobTargetState(name string, target job.JobState) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) CheckJobPulse(jobName string) (string, bool) {
-	return "", false
+func (t *TestRegistry) CheckJobPulse(jobName string) (string, bool) {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) CreateJob(j *job.Job) error {
-	return nil
+func (t *TestRegistry) CreateJob(j *job.Job) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) DestroyJob(name string) error {
-	return nil
+func (t *TestRegistry) DestroyJob(name string) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) CreateSignatureSet(s *sign.SignatureSet) error {
-	return nil
+func (t *TestRegistry) CreateSignatureSet(s *sign.SignatureSet) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) GetJobTarget(name string) (string, error) {
+func (t *TestRegistry) GetJobTarget(name string) (string, error) {
 	js := t.jobStates[name]
 	if js != nil {
 		return js.MachineState.ID, nil
@@ -69,7 +69,7 @@ func (t TestRegistry) GetJobTarget(name string) (string, error) {
 	return "", nil
 }
 
-func (t TestRegistry) GetMachineState(machID string) (*machine.MachineState, error) {
+func (t *TestRegistry) GetMachineState(machID string) (*machine.MachineState, error) {
 	for _, ms := range t.machines {
 		if ms.ID == machID {
 			return &ms, nil
@@ -78,88 +78,88 @@ func (t TestRegistry) GetMachineState(machID string) (*machine.MachineState, err
 	return nil, nil
 }
 
-func (t TestRegistry) GetDebugInfo() (string, error) {
+func (t *TestRegistry) GetDebugInfo() (string, error) {
 	return "", nil
 }
 
-func (t TestRegistry) ClearJobHeartbeat(jobName string) {
-	return
+func (t *TestRegistry) ClearJobHeartbeat(jobName string) {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) ClearJobTarget(jobName, machID string) error {
-	return nil
+func (t *TestRegistry) ClearJobTarget(jobName, machID string) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) CreateJobOffer(jo *job.JobOffer) error {
-	return nil
+func (t *TestRegistry) CreateJobOffer(jo *job.JobOffer) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) DestroySignatureSet(tag string) {
-	return
+func (t *TestRegistry) DestroySignatureSet(tag string) {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) GetJobTargetState(jobName string) (*job.JobState, error) {
-	return nil, nil
+func (t *TestRegistry) GetJobTargetState(jobName string) (*job.JobState, error) {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) GetSignatureSet(tag string) *sign.SignatureSet {
-	return nil
+func (t *TestRegistry) GetSignatureSet(tag string) *sign.SignatureSet {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) GetSignatureSetOfJob(name string) (*sign.SignatureSet, error) {
-	return nil, nil
+func (t *TestRegistry) GetSignatureSetOfJob(name string) (*sign.SignatureSet, error) {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) JobHeartbeat(jobName, agentMachID string, ttl time.Duration) error {
-	return nil
+func (t *TestRegistry) JobHeartbeat(jobName, agentMachID string, ttl time.Duration) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) LockJob(jobName, context string) *TimedResourceMutex {
-	return nil
+func (t *TestRegistry) LockJob(jobName, context string) *TimedResourceMutex {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) LockJobOffer(jobName, context string) *TimedResourceMutex {
-	return nil
+func (t *TestRegistry) LockJobOffer(jobName, context string) *TimedResourceMutex {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) LockMachine(machID, context string) *TimedResourceMutex {
-	return nil
+func (t *TestRegistry) LockMachine(machID, context string) *TimedResourceMutex {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) RemoveMachineState(machID string) error {
-	return nil
+func (t *TestRegistry) RemoveMachineState(machID string) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) RemoveUnitState(jobName string) error {
-	return nil
+func (t *TestRegistry) RemoveUnitState(jobName string) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) ResolveJobOffer(jobName string) error {
-	return nil
+func (t *TestRegistry) ResolveJobOffer(jobName string) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) SaveUnitState(jobName string, unitState *unit.UnitState) {
-	return
+func (t *TestRegistry) SaveUnitState(jobName string, unitState *unit.UnitState) {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) ScheduleJob(jobName string, machID string) error {
-	return nil
+func (t *TestRegistry) ScheduleJob(jobName string, machID string) error {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) SetMachineState(ms machine.MachineState, ttl time.Duration) (uint64, error) {
-	return 0, nil
+func (t *TestRegistry) SetMachineState(ms machine.MachineState, ttl time.Duration) (uint64, error) {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) SubmitJobBid(jb *job.JobBid) {
-	return
+func (t *TestRegistry) SubmitJobBid(jb *job.JobBid) {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) UnresolvedJobOffers() []job.JobOffer {
-	return nil
+func (t *TestRegistry) UnresolvedJobOffers() []job.JobOffer {
+	panic("Not implemented")
 }
 
-func (t TestRegistry) Bids(jo *job.JobOffer) ([]job.JobBid, error) {
-	return nil, nil
+func (t *TestRegistry) Bids(jo *job.JobOffer) ([]job.JobBid, error) {
+	panic("Not implemented")
 }
 
 func (t TestRegistry) GetLatestVersion() (*semver.Version, error) {
