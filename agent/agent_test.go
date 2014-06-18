a2923e29d69370cd5b3778b0d3992f3e3ff6154c
Jonathan Boulle <jonathanboulle@gmail.com>
2014-06-18 12:01:46 -0700

    chore(agent): centralize NewJob/NewUnit invocations in agent test

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -9,79 +9,61 @@ import (
 	"github.com/coreos/fleet/unit"
 )
 
-func newTestJobWithMachineMetadata(metadata string) *job.Job {
+func newTestJobFromUnitContents(t *testing.T, name, contents string) *job.Job {
+	u, err := unit.NewUnit(contents)
+	if err != nil {
+		t.Fatalf("error creating Unit from %q: %v", contents, err)
+	}
+	j := job.NewJob(name, *u)
+	if j == nil {
+		t.Fatalf("error creating Job %q from %q", name, u)
+	}
+	return j
+}
+
+func newTestJobWithXFleetValues(t *testing.T, metadata string) *job.Job {
 	contents := fmt.Sprintf(`
 [X-Fleet]
 %s
 `, metadata)
-	u, _ := unit.NewUnit(contents)
-
-	return job.NewJob("pong.service", *u)
+	return newTestJobFromUnitContents(t, "pong.service", contents)
 }
 
-func TestAbleToRunConditionMachineIDMatch(t *testing.T) {
-	u, _ := unit.NewUnit(`[X-Fleet]
-X-ConditionMachineID=XYZ
-`)
-	job := job.NewJob("example.service", *u)
+func TestAbleToRunConditionMachineID(t *testing.T) {
+	for id, want := range map[string]bool{
+		"XYZ": true,
+		"123": false,
+	} {
+		job := newTestJobWithXFleetValues(t, "X-ConditionMachineID=XYZ")
+		mach := &machine.FakeMachine{machine.MachineState{ID: id}}
+		agent := Agent{Machine: mach, state: NewState()}
+		got := agent.ableToRun(job)
+		if got != want {
+			t.Errorf("Bad ableToRun for machineID %q: got %t, want %t", id, got, want)
+		}
 
-	mach := &machine.FakeMachine{machine.MachineState{ID: "XYZ"}}
-	agent := Agent{Machine: mach, state: NewState()}
-	if !agent.ableToRun(job) {
-		t.Fatalf("Agent should be able to run job")
 	}
 }
 
-func TestAbleToRunConditionMachineIDMismatch(t *testing.T) {
-	u, _ := unit.NewUnit(`[X-Fleet]
-X-ConditionMachineID=XYZ
-`)
-	job := job.NewJob("example.service", *u)
-
-	mach := &machine.FakeMachine{machine.MachineState{ID: "123"}}
-	agent := Agent{Machine: mach, state: NewState()}
-	if agent.ableToRun(job) {
-		t.Fatalf("Agent should not be able to run job")
-	}
-}
-
-// Assert that an existing conflict is triggered against the potential job name
-func TestHasConflictExistingMatch(t *testing.T) {
-	state := NewState()
-
-	u, _ := unit.NewUnit(`[X-Fleet]
-X-Conflicts=other.service
-`)
-	j := job.NewJob("example.service", *u)
-	state.TrackJob(j)
-	state.SetTargetState(j.Name, job.JobStateLoaded)
-
-	agent := Agent{state: state}
-
-	matched, name := agent.HasConflict("other.service", []string{})
-	if !matched {
-		t.Errorf("Expected conflict with 'example.service', no conflict reported")
-	} else if name != "example.service" {
-		t.Errorf("Expected conflict with 'example.service', but conflict found with %s", name)
-	}
-}
-
-// Assert that a potential conflict is triggered against the existing job name
-func TestHasConflictPotentialMatch(t *testing.T) {
-	state := NewState()
-
-	u, _ := unit.NewUnit(`[X-Fleet]`)
-	j := job.NewJob("example.service", *u)
-	state.TrackJob(j)
-	state.SetTargetState(j.Name, job.JobStateLoaded)
-
-	agent := Agent{state: state}
-
-	matched, name := agent.HasConflict("other.service", []string{"example.service"})
-	if !matched {
-		t.Errorf("Expected conflict with 'example.service', no conflict reported")
-	} else if name != "example.service" {
-		t.Errorf("Expected conflict with 'example.service', but conflict found with %s", name)
+func TestHasConflictMatches(t *testing.T) {
+	for i, tt := range []struct {
+		contents           string
+		potentialConflicts []string
+	}{
+		{"[X-Fleet]\nX-Conflicts=other.service", []string{}},
+		{"[X-Fleet]", []string{"example.service"}},
+	} {
+		state := NewState()
+		j := newTestJobFromUnitContents(t, "example.service", tt.contents)
+		state.TrackJob(j)
+		state.SetTargetState(j.Name, job.JobStateLoaded)
+		agent := Agent{state: state}
+		matched, name := agent.HasConflict("other.service", []string{"example.service"})
+		if !matched {
+			t.Errorf("%d: Expected conflict with 'example.service', no conflict reported", i)
+		} else if name != "example.service" {
+			t.Errorf("%d: Expected conflict with 'example.service', but conflict found with %s", i, name)
+		}
 	}
 }
 
@@ -89,9 +71,7 @@ func TestHasConflictPotentialMatch(t *testing.T) {
 // trigger a match
 func TestHasConflictNoMatch(t *testing.T) {
 	state := NewState()
-
-	u, _ := unit.NewUnit(`[X-Fleet]`)
-	j := job.NewJob("example.service", *u)
+	j := newTestJobFromUnitContents(t, "example.service", "[X-Fleet]")
 	state.TrackJob(j)
 	state.SetTargetState(j.Name, job.JobStateLoaded)
 
@@ -107,31 +87,27 @@ func TestHasConflictNoMatch(t *testing.T) {
 func TestHasConflictComplexGlob(t *testing.T) {
 	state := NewState()
 
-	u, _ := unit.NewUnit(`[X-Fleet]
-X-Conflicts=*.[1-9].service
-`)
-	j := job.NewJob("example.service", *u)
+	j := newTestJobWithXFleetValues(t, "X-Conflicts=*.[1-9].service")
 	state.TrackJob(j)
 	state.SetTargetState(j.Name, job.JobStateLoaded)
 
 	agent := Agent{state: state}
 
-	matched, name := agent.HasConflict("other.2.service", []string{})
-	if !matched {
-		t.Errorf("Expected conflict with 'example.service', but no conflict reported")
-	} else if name != "example.service" {
-		t.Errorf("Expected conflict with 'example.service', but conflict found with %s", name)
+	for _, conflict := range []string{"other.2.service", "foo.1.service", ".9.service"} {
+		matched, name := agent.HasConflict(conflict, []string{})
+		if !matched {
+			t.Errorf("Expected %q to have conflict with %q, but no conflict reported", conflict, j.Name)
+		} else if name != j.Name {
+			t.Errorf("Expected %q to have conflict with %q, but conflict found with %s", conflict, j.Name, name)
+		}
 	}
 }
 
 func TestHasConflictIgnoresUnscheduledJobs(t *testing.T) {
 	state := NewState()
-
-	u, _ := unit.NewUnit(`[X-Fleet]
-X-Conflicts=other.service
-`)
-	j := job.NewJob("example.service", *u)
+	j := newTestJobWithXFleetValues(t, "X-Conflicts=other.service")
 	state.TrackJob(j)
+
 	state.SetTargetState(j.Name, job.JobStateInactive)
 
 	agent := Agent{state: state}
@@ -144,12 +120,9 @@ X-Conflicts=other.service
 
 func TestHasConflictIgnoresBids(t *testing.T) {
 	state := NewState()
-
-	u, _ := unit.NewUnit(`[X-Fleet]
-X-Conflicts=other.service
-`)
-	j := job.NewJob("example.service", *u)
+	j := newTestJobWithXFleetValues(t, "X-Conflicts=other.service")
 	state.TrackJob(j)
+
 	state.TrackBid(j.Name)
 
 	agent := Agent{state: state}
@@ -185,7 +158,7 @@ X-ConditionMachineMetadata=region=us-west-1`, true},
 	agent := &Agent{Machine: &machine.FakeMachine{ms}, state: NewState()}
 
 	for i, e := range metadataAbleToRunExamples {
-		job := newTestJobWithMachineMetadata(e.C)
+		job := newTestJobWithXFleetValues(t, e.C)
 		g := agent.ableToRun(job)
 		if g != e.A {
 			t.Errorf("Unexpected output %d, content: %q\n\tgot %t, want %t\n", i, e.C, g, e.A)
