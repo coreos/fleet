e65628014fedf8e4ceec2e399f7cf48ce6d8ba09
Brian Waldon <bcwaldon@gmail.com>
2014-06-24 09:24:21 -0700

    refactor(tests): Use table-driven testing for units resource

--- api/units_test.go
+++ api/units_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha1"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -177,125 +178,165 @@ func TestMapJobToSchema(t *testing.T) {
 }
 
 func TestUnitGet(t *testing.T) {
+	tests := []struct {
+		item string
+		code int
+	}{
+		{item: "XXX", code: http.StatusOK},
+		{item: "ZZZ", code: http.StatusNotFound},
+	}
+
 	fr := registry.NewFakeRegistry()
 	fr.SetJobs([]job.Job{
 		{Name: "XXX"},
 		{Name: "YYY"},
 	})
 	resource := &unitsResource{fr, "/units"}
-	rw := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "http://example.com/units/XXX", nil)
-	if err != nil {
-		t.Fatalf("Failed creating http.Request: %v", err)
-	}
 
-	resource.get(rw, req, "XXX")
-	if rw.Code != http.StatusOK {
-		t.Errorf("Expected 200, got %d", rw.Code)
-	}
-
-	ct := rw.HeaderMap["Content-Type"]
-	if len(ct) != 1 {
-		t.Errorf("Response has wrong number of Content-Type values: %v", ct)
-	} else if ct[0] != "application/json" {
-		t.Errorf("Expected application/json, got %s", ct)
-	}
-
-	if rw.Body == nil {
-		t.Error("Received nil response body")
-	} else {
-		var unit schema.Unit
-		err := json.Unmarshal(rw.Body.Bytes(), &unit)
+	for i, tt := range tests {
+		rw := httptest.NewRecorder()
+		req, err := http.NewRequest("GET", fmt.Sprintf("http://example.com/units/%s", tt.item), nil)
 		if err != nil {
-			t.Fatalf("Received unparseable body: %v", err)
+			t.Errorf("case %d: failed creating http.Request: %v", i, err)
+			continue
 		}
 
-		if unit.Name != "XXX" {
-			t.Errorf("Received incorrect Unit entity: %v", unit)
+		resource.get(rw, req, tt.item)
+		if tt.code != rw.Code {
+			t.Errorf("case %d: expected %d, got %d", i, tt.code, rw.Code)
 		}
 	}
 }
 
 func TestUnitsDestroy(t *testing.T) {
-	fr := registry.NewFakeRegistry()
-	fr.SetJobs([]job.Job{
-		{Name: "XXX"},
-		{Name: "YYY"},
-		{Name: "ZZZ"},
-	})
-	resource := &unitsResource{fr, "/units"}
-	rw := httptest.NewRecorder()
-	req, err := http.NewRequest("DELETE", "http://example.com/units/YYY", nil)
-	if err != nil {
-		t.Fatalf("Failed creating http.Request: %v", err)
+	tests := []struct {
+		// initial state of registry
+		init []job.Job
+		// which Job to attempt to delete
+		arg string
+		// expected HTTP status code
+		code int
+		// expected state of registry after deletion attempt
+		remaining []string
+	}{
+		{
+			init:      []job.Job{job.Job{Name: "XXX", Unit: unit.Unit{Raw: "FOO"}}},
+			arg:       "XXX",
+			code:      http.StatusNoContent,
+			remaining: []string{},
+		},
 	}
 
-	resource.destroy(rw, req, "YYY")
-	if rw.Code != http.StatusNoContent {
-		t.Errorf("Expected 204, got %d", rw.Code)
-	}
+	for i, tt := range tests {
+		fr := registry.NewFakeRegistry()
+		fr.SetJobs(tt.init)
 
-	jobs, err := fr.Jobs()
-	if err != nil {
-		t.Fatalf("Failed fetching Jobs after destruction: %v", err)
-	}
+		req, err := http.NewRequest("DELETE", fmt.Sprintf("http://example.com/units/%s", tt.arg), nil)
+		if err != nil {
+			t.Errorf("case %d: failed creating http.Request: %v", i, err)
+			continue
+		}
 
-	remaining := make([]string, len(jobs))
-	for i, j := range jobs {
-		remaining[i] = j.Name
-	}
+		resource := &unitsResource{fr, "/units"}
+		rw := httptest.NewRecorder()
+		resource.destroy(rw, req, tt.arg)
+		if tt.code != rw.Code {
+			t.Errorf("case %d: expected %d, got %d", i, tt.code, rw.Code)
+		}
 
-	expect := []string{"XXX", "ZZZ"}
-	if !reflect.DeepEqual(remaining, expect) {
-		t.Errorf("Expected Jobs %v, got %v", expect, remaining)
+		jobs, err := fr.Jobs()
+		if err != nil {
+			t.Errorf("case %d: failed fetching Jobs after destruction: %v", i, err)
+			continue
+		}
+
+		remaining := make([]string, len(jobs))
+		for i, j := range jobs {
+			remaining[i] = j.Name
+		}
+
+		if !reflect.DeepEqual(tt.remaining, remaining) {
+			t.Errorf("case %d: expected Jobs %v, got %v", i, tt.remaining, remaining)
+		}
 	}
 }
 
 func TestUnitsSetDesiredState(t *testing.T) {
-	fr := registry.NewFakeRegistry()
-	fr.SetJobs([]job.Job{
-		{Name: "XXX"},
-		{Name: "YYY"},
-	})
-	fr.SetJobTargetState("XXX", "launched")
-	fr.SetJobTargetState("YYY", "inactive")
-
-	resource := &unitsResource{fr, "/units"}
-	rw := httptest.NewRecorder()
-	req, err := http.NewRequest("POST", "http://example.com", nil)
-	if err != nil {
-		t.Fatalf("Failed creating http.Request: %v", err)
+	tests := []struct {
+		// initial state of Registry
+		initJobs   []job.Job
+		initStates map[string]job.JobState
+		// which Job to attempt to delete
+		arg schema.DesiredUnitState
+		// expected HTTP status code
+		code int
+		// expected state of registry after request
+		finalStates map[string]job.JobState
+	}{
+		// Modify the DesiredState of an existing Job
+		{
+			initJobs:    []job.Job{job.Job{Name: "XXX", Unit: unit.Unit{Raw: "FOO"}}},
+			initStates:  map[string]job.JobState{"XXX": "inactive"},
+			arg:         schema.DesiredUnitState{Name: "XXX", DesiredState: "launched"},
+			code:        http.StatusNoContent,
+			finalStates: map[string]job.JobState{"XXX": "launched"},
+		},
+		// Create a new Job
+		{
+			initJobs:    []job.Job{},
+			initStates:  map[string]job.JobState{},
+			arg:         schema.DesiredUnitState{Name: "YYY", DesiredState: "loaded", FileContents: "cGVubnkNCg=="},
+			code:        http.StatusNoContent,
+			finalStates: map[string]job.JobState{"YYY": "loaded"},
+		},
 	}
 
-	body := schema.DesiredUnitState{Name: "XXX", DesiredState: "loaded"}
-	enc, err := json.Marshal(body)
-	if err != nil {
-		t.Fatalf("Unable to JSON-encode request: %v", err)
-	}
-	req.Body = ioutil.NopCloser(bytes.NewBuffer(enc))
-	req.Header.Set("Content-Type", "application/json")
+	for i, tt := range tests {
+		fr := registry.NewFakeRegistry()
+		fr.SetJobs(tt.initJobs)
+		for j, s := range tt.initStates {
+			err := fr.SetJobTargetState(j, s)
+			if err != nil {
+				t.Errorf("case %d: failed initializing Job target state: %v", err)
+			}
+		}
 
-	resource.set(rw, req, "XXX")
-	if rw.Code != http.StatusNoContent {
-		t.Errorf("Expected 204, got %d", rw.Code)
-	}
-
-	expect := map[string]string{
-		"XXX": "loaded",
-		"YYY": "inactive",
-	}
-	for name, state := range expect {
-		j, _ := fr.Job(name)
-		if j == nil {
-			t.Errorf("Expected job %s to exist, got nil", j.Name)
+		req, err := http.NewRequest("PUT", fmt.Sprintf("http://example.com/units/%s", tt.arg.Name), nil)
+		if err != nil {
+			t.Errorf("case %d: failed creating http.Request: %v", i, err)
 			continue
 		}
 
-		ts, _ := fr.JobTargetState(name)
-		if ts == nil {
-			t.Errorf("Expected job %s to have target state %v, got nil", j, state)
-		} else if sts := string(*ts); sts != state {
-			t.Errorf("Expected job %s to have target state %v, got %v", j, state, sts)
+		enc, err := json.Marshal(tt.arg)
+		if err != nil {
+			t.Errorf("case %d: unable to JSON-encode request: %v", i, err)
+			continue
+		}
+		req.Body = ioutil.NopCloser(bytes.NewBuffer(enc))
+		req.Header.Set("Content-Type", "application/json")
+
+		resource := &unitsResource{fr, "/units"}
+		rw := httptest.NewRecorder()
+		resource.set(rw, req, tt.arg.Name)
+		if tt.code != rw.Code {
+			t.Errorf("case %d: expected %d, got %d", i, tt.code, rw.Code)
+		}
+
+		for name, expect := range tt.finalStates {
+			j, err := fr.Job(name)
+			if err != nil {
+				t.Errorf("case %d: failed fetching Job: %v", i, err)
+			} else if j == nil {
+				t.Errorf("case %d: fetched nil Job(%s), expected non-nil", i, name)
+			}
+			finalState, err := fr.JobTargetState(name)
+			if err != nil {
+				t.Errorf("case %d: failed fetching target JobState: %v", i, err)
+			} else if finalState == nil {
+				t.Errorf("case %d: got nil target state for Job(%s), expected non-nil", i, name)
+			} else if *finalState != expect {
+				t.Errorf("case %d: expect Job(%s) target state %q, got %q", i, name, expect, *finalState)
+			}
 		}
 	}
 }
