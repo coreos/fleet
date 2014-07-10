3d74733a5dc5669588e4e3973991203d8a1a947e
Brian Waldon <bcwaldon@gmail.com>
2014-07-10 11:56:14 -0700

    unit: switch to go-systemd unit deserializer

--- unit/file.go
+++ unit/file.go
@@ -2,26 +2,47 @@ package unit
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"strings"
+
+	"github.com/coreos/fleet/Godeps/_workspace/src/github.com/coreos/go-systemd/unit"
 )
 
-// Description returns the first Description option found in the [Unit] section.
-// If the option is not defined, an empty string is returned.
-func (u *Unit) Description() string {
-	if values := u.Contents["Unit"]["Description"]; len(values) > 0 {
-		return values[0]
-	}
-	return ""
-}
-
 func NewUnit(raw string) (*Unit, error) {
-	parsed, err := deserializeUnitFile(raw)
+	reader := strings.NewReader(raw)
+	opts, err := unit.Deserialize(reader)
 	if err != nil {
 		return nil, err
 	}
-	return &Unit{parsed, raw}, nil
+
+	contents := mapOptions(opts)
+	return &Unit{contents, raw}, nil
+}
+
+func mapOptions(opts []*unit.UnitOption) map[string]map[string][]string {
+	contents := make(map[string]map[string][]string)
+	for _, opt := range opts {
+		if _, ok := contents[opt.Section]; !ok {
+			contents[opt.Section] = make(map[string][]string)
+		}
+
+		if _, ok := contents[opt.Section][opt.Name]; !ok {
+			contents[opt.Section][opt.Name] = make([]string, 0)
+		}
+
+		var vals []string
+		if opt.Section == "X-Fleet" {
+			// The go-systemd parser does not know that our X-Fleet options support
+			// multivalue options, so we have to manually parse them here
+			vals = parseMultivalueLine(opt.Value)
+		} else {
+			vals = []string{opt.Value}
+		}
+
+		contents[opt.Section][opt.Name] = append(contents[opt.Section][opt.Name], vals...)
+	}
+
+	return contents
 }
 
 // NewUnitFromLegacyContents creates a Unit object from an obsolete unit
