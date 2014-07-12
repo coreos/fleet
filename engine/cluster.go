8eaa869b39336f42a8bffb0672f75602ae3fa6ad
Jonathan Boulle <jonathanboulle@gmail.com>
2014-07-11 23:37:00 -0700

    engine/cluster: add proper job tracking to cluster, including peers, conflicts, and job<->machine mappings

--- engine/cluster.go
+++ engine/cluster.go
@@ -22,6 +22,10 @@ type ClusterCache interface {
 	// TrackMachine adds the given machine to the cluster view.
 	TrackMachine(m *machine.MachineState)
 
+	// AddJob adds the given active Job to the cluster view, tracking its
+	// currently scheduled location
+	AddJob(j *job.Job)
+
 	// MachinePresent determines if the referenced Machine appears to be a
 	// current member of the cluster.
 	MachinePresent(machID string) bool
@@ -31,21 +35,31 @@ type ClusterCache interface {
 	// sorted by ascending lexicographical string value.
 	Candidates(j *job.Job) []string
 
-	// AddJob adds the given Job to the cluster view; it is intended to be
-	// used during the engine's reconcilation process to ensure that Job
-	// scheduling decisions are reflected in the state of the cluster.
-	AddJob(machID string, j *job.Job)
+	// ScheduleJob updates the cluster with the given Job scheduling
+	// decision by mutating the referenced machine's MachineState; it is
+	// intended to be used during the engine's reconcilation process to
+	// ensure that Job scheduling decisions are reflected in the state of
+	// the cluster.
+	ScheduleJob(machID string, j *job.Job)
 }
 
 // cluster is the canonical ClusterCache implementation providing basic
 // resource-based scheduling. cluster is NOT thread-safe.
 type cluster struct {
-	machines map[string]*machine.MachineState
+	machines     map[string]*machine.MachineState
+	machsToJobs  map[string][]string
+	jobToMach    map[string]string
+	jobConflicts map[string][]string
+	jobPeers     map[string][]string
 }
 
 func newCluster() ClusterCache {
 	return &cluster{
-		machines: make(map[string]*machine.MachineState),
+		machines:     make(map[string]*machine.MachineState),
+		machsToJobs:  make(map[string][]string),
+		jobToMach:    make(map[string]string),
+		jobConflicts: make(map[string][]string),
+		jobPeers:     make(map[string][]string),
 	}
 }
 
@@ -53,25 +67,55 @@ func (c *cluster) TrackMachine(m *machine.MachineState) {
 	c.machines[m.ID] = m
 }
 
+func (c *cluster) AddJob(j *job.Job) {
+	mID := j.TargetMachineID
+	m := c.machsToJobs[mID]
+	if m == nil {
+		m = make([]string, 0)
+	}
+	m = append(m, j.Name)
+	c.machsToJobs[mID] = m
+	c.jobToMach[j.Name] = mID
+	// Since this Job is already scheduled to a machine, its resources are
+	// captured by that machine's FreeResources, so no need to track them
+	// here. However, we do need to track its peers and conflicts for other
+	// scheduling decisions.
+	c.trackJobPeers(j.Name, j.Peers())
+	c.trackJobConflicts(j.Name, j.Conflicts())
+}
+
 func (c *cluster) MachinePresent(machID string) bool {
 	_, ok := c.machines[machID]
 	return ok
 }
 
-// AddJob updates the FreeResources of the MachineState by the given
+// ScheduleJob updates the FreeResources of the MachineState by the given
 // machine ID by subtracting the resource reservation of the given Job and
 // incrementing the LoadedUnits count
-func (c *cluster) AddJob(machID string, j *job.Job) {
+func (c *cluster) ScheduleJob(machID string, j *job.Job) {
 	old := c.machines[machID].FreeResources
 	c.machines[machID].FreeResources = resource.Sub(old, j.Resources())
 	c.machines[machID].LoadedUnits++
 }
 
-// Candidates determines which machines are eligible to run the given Job,
-// based on resource requirements and the current load in the cluster
+// Candidates determines which machines are eligible to run the given Job(s),
+// based on job requirements, resource requirements, and the current load in
+// the cluster
 func (c *cluster) Candidates(j *job.Job) []string {
 	if machID, ok := j.RequiredTarget(); ok {
+		// TODO(jonboulle): should we care about resource requirements and other restrictions in this case?
 		return []string{machID}
+	}
+
+	if len(j.Peers()) > 0 {
+		// TODO(jonboulle): deal with this. we need to schedule peerGroup as a unit.
+		// peerGroup := c.resolvePeers(jName)
+		return []string{}
+	}
+
+	if len(j.Conflicts()) > 0 {
+		// TODO(jonboulle): deal with this. including transitive conflicts.
+		return []string{}
 	}
 
 	var machineIDs []string
@@ -152,4 +196,52 @@ func (c *cluster) sufficientResources(req resource.ResourceTuple) []string {
 		return []string{}
 	}
 	return []string{ms[j].ID}
+}
+
+// trackJobConflicts stores the conflicts of a Job
+func (c *cluster) trackJobConflicts(jName string, conflicts []string) {
+	c.jobConflicts[jName] = conflicts
+}
+
+// trackJobPeers stores the bi-directional dependency between a Job
+// and its peers:
+//    1 Job --> N Peers
+//    N Peers --> 1 Job
+func (c *cluster) trackJobPeers(jName string, peers []string) {
+	if _, ok := c.jobPeers[jName]; !ok {
+		c.jobPeers[jName] = make([]string, 0)
+
+	}
+	c.jobPeers[jName] = append(c.jobPeers[jName], peers...)
+
+	for _, peer := range peers {
+		if _, ok := c.jobPeers[peer]; !ok {
+			c.jobPeers[peer] = make([]string, 0)
+		}
+		c.jobPeers[peer] = append(c.jobPeers[peer], jName)
+	}
+}
+
+// resolvePeers determines the transitive closure of all Peers of the
+// Job of a given Name, sorted lexicographically
+func (c *cluster) resolvePeers(jName string) []string {
+	seen := make(map[string]bool)
+	all := make([]string, 0)
+	c.resolve(jName, seen)
+	for j, _ := range seen {
+		all = append(all, j)
+	}
+	sort.Strings(all)
+	return all
+}
+
+func (c *cluster) resolve(name string, seen map[string]bool) {
+	if _, ok := seen[name]; ok {
+		return
+	}
+	seen[name] = true
+	for _, peer := range c.jobPeers[name] {
+		c.resolve(peer, seen)
+	}
+	return
 }
--- engine/cluster_test.go
+++ engine/cluster_test.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"fmt"
 	"reflect"
 	"sort"
 	"testing"
@@ -62,6 +63,11 @@ func TestByResources(t *testing.T) {
 	}
 }
 
+func newEmptyTestCluster() *cluster {
+	c := newCluster()
+	return c.(*cluster)
+}
+
 func newTestCluster() *cluster {
 	c := &cluster{
 		machines: make(map[string]*machine.MachineState),
@@ -75,7 +81,7 @@ func newTestCluster() *cluster {
 		})
 	}
 	for _, m := range ms {
-		c.machines[m.ID] = m
+		c.TrackMachine(m)
 	}
 	return c
 }
@@ -146,4 +152,172 @@ func TestCandidates(t *testing.T) {
 			t.Errorf("case %d: got %s, want %s", i, got, tt.want)
 		}
 	}
+}
+
+func TestTrackJobPeers(t *testing.T) {
+	c := newEmptyTestCluster()
+
+	// Add a single Job with peers
+	c.trackJobPeers("foo", []string{"bar", "baz"})
+
+	want := map[string][]string{
+		"foo": []string{"bar", "baz"},
+		"bar": []string{"foo"},
+		"baz": []string{"foo"},
+	}
+
+	got := c.jobPeers
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("initial trackJobPeers failed got %v, want %v", got, want)
+	}
+
+	// Add a separate Job with an existing common peer
+	c.trackJobPeers("woof", []string{"quack", "foo"})
+
+	want = map[string][]string{
+		"foo":   []string{"bar", "baz", "woof"},
+		"bar":   []string{"foo"},
+		"baz":   []string{"foo"},
+		"woof":  []string{"quack", "foo"},
+		"quack": []string{"woof"},
+	}
+
+	got = c.jobPeers
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("second trackJobPeers failed got %v, want %v", got, want)
+	}
+
+	c.trackJobPeers("baz", []string{"quack", "meow"})
+
+	want = map[string][]string{
+		"foo":   []string{"bar", "baz", "woof"},
+		"bar":   []string{"foo"},
+		"baz":   []string{"foo", "quack", "meow"},
+		"woof":  []string{"quack", "foo"},
+		"quack": []string{"woof", "baz"},
+		"meow":  []string{"baz"},
+	}
+
+	got = c.jobPeers
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("final trackJobPeers failed got %v, want %v", got, want)
+	}
+}
+
+func TestTrackJobConflicts(t *testing.T) {
+	c := newEmptyTestCluster()
+	c.trackJobConflicts("foo", []string{"bar", "baz"})
+	want := []string{"bar", "baz"}
+	got := c.jobConflicts["foo"]
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("trackJobConflicts: got %v, want %v", got, want)
+	}
+
+}
+
+func newScheduledTestJobWithXFleet(t *testing.T, name, machine, metadata string) *job.Job {
+	contents := fmt.Sprintf(`
+[X-Fleet]
+%s
+`, metadata)
+	u, err := unit.NewUnit(contents)
+	if err != nil {
+		t.Fatalf("error creating Unit from %q: %v", contents, err)
+	}
+	j := job.NewJob(name, *u)
+	if j == nil {
+		t.Fatalf("error creating Job %q from %q", name, u)
+	}
+	j.TargetMachineID = machine
+	return j
+}
+
+func TestAddJob(t *testing.T) {
+	c := newEmptyTestCluster()
+	j1 := newScheduledTestJobWithXFleet(t, "j1", "m1", `XConditionMachineOf=j2`)
+	j2 := newScheduledTestJobWithXFleet(t, "j2", "m1", ``)
+	j3 := newScheduledTestJobWithXFleet(t, "j3", "m2", `XConflicts=j4`)
+	j4 := newScheduledTestJobWithXFleet(t, "j4", `m3`, ``)
+
+	c.AddJob(j1)
+	c.AddJob(j2)
+	c.AddJob(j3)
+	c.AddJob(j4)
+
+	wantm := map[string][]string{
+		"m1": []string{"j1", "j2"},
+		"m2": []string{"j3"},
+		"m3": []string{"j4"},
+	}
+	gotm := c.machsToJobs
+	if !reflect.DeepEqual(gotm, wantm) {
+		t.Errorf("bad machsToJobs: got %v, want %v", gotm, wantm)
+	}
+
+	wantj := map[string]string{
+		"j1": "m1",
+		"j2": "m1",
+		"j3": "m2",
+		"j4": "m3",
+	}
+	gotj := c.jobToMach
+	if !reflect.DeepEqual(gotj, wantj) {
+		t.Errorf("bad jobToMach: got %v, want %v", gotj, wantj)
+	}
+}
+
+func TestMachinePresent(t *testing.T) {
+	c := newEmptyTestCluster()
+	if c.MachinePresent("miyagi") {
+		t.Errorf("MachinePresent returned true for no machines?!")
+	}
+	c.TrackMachine(&machine.MachineState{ID: "miyagi"})
+	if !c.MachinePresent("miyagi") {
+		t.Errorf("MachinePresent returned false unexpectedly")
+	}
+	if c.MachinePresent("daniel") {
+		t.Errorf("MachinePresent returned true unexpectedly")
+	}
+}
+
+func TestResolvePeers(t *testing.T) {
+	c := &cluster{
+		jobPeers: map[string][]string{
+			"foo":   {"bar", "baz", "woof"},
+			"bar":   {"foo"},
+			"baz":   {"foo", "quack", "meow"},
+			"woof":  {"quack", "foo"},
+			"quack": {"woof", "baz"},
+			"meow":  {"baz"},
+			"bark":  {"yap"},
+			"yap":   {"bark"},
+		},
+	}
+	for i, tt := range []struct {
+		jName string
+		want  []string
+	}{
+		{
+			"foo",
+			[]string{"bar", "baz", "foo", "meow", "quack", "woof"},
+		},
+		{
+			"quack",
+			[]string{"bar", "baz", "foo", "meow", "quack", "woof"},
+		},
+		{
+			"bark",
+			[]string{"bark", "yap"},
+		},
+		{
+			"yap",
+			[]string{"bark", "yap"},
+		},
+	} {
+		got := c.resolvePeers(tt.jName)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("case %d: got %v, want %v", i, got, tt.want)
+		}
+	}
+
 }
