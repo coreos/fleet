d249a347b2e40314e17812efbf9d67b295e44667
Brian Waldon <bcwaldon@gmail.com>
2014-09-02 09:59:08 -0700

    agent: refactor UnitStatePublisher locking + timing
    - rename addToCache --> updateCache
    - split logic in publishAll into pruneCache and Run
    - add test coverage for updateCache & pruneCache
    - updateCache checks if state existed previously in cache and if not,
      publishes immediately (i.e. considers it a change)
    - Run waits TTL/2 between publishing entire state
    - Run only locks the cache to copy it; no longer holds lock while
      publishing to etcd

--- agent/unit_state.go
+++ agent/unit_state.go
@@ -35,13 +35,17 @@ type UnitStatePublisher struct {
 // are received on the channel.
 func (p *UnitStatePublisher) Run(beatchan <-chan *unit.UnitStateHeartbeat, stop chan bool) {
 	go func() {
-		tick := time.Tick(5 * time.Second)
 		for {
 			select {
 			case <-stop:
 				return
-			case <-tick:
-				p.publishAll()
+			case <-time.After(p.ttl / 2):
+				p.mutex.Lock()
+				for name, us := range p.cache {
+					p.publishOne(name, us)
+				}
+				p.pruneCache()
+				p.mutex.Unlock()
 			}
 		}
 	}()
@@ -57,24 +61,19 @@ func (p *UnitStatePublisher) Run(beatchan <-chan *unit.UnitStateHeartbeat, stop 
 				bt.State.MachineID = machID
 			}
 
-			p.addToCache(bt)
+			if p.updateCache(bt) {
+				go p.publishOne(bt.Name, bt.State)
+			}
 		}
 	}
 }
 
-func (p *UnitStatePublisher) publishAll() {
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
-
-	cache := make(map[string]*unit.UnitState)
+func (p *UnitStatePublisher) pruneCache() {
 	for name, us := range p.cache {
-		p.publishOne(name, us)
-		if us != nil {
-			cache[name] = us
+		if us == nil {
+			delete(p.cache, name)
 		}
 	}
-
-	p.cache = cache
 }
 
 func (p *UnitStatePublisher) publishOne(name string, us *unit.UnitState) {
@@ -102,17 +101,18 @@ func (p *UnitStatePublisher) publishOne(name string, us *unit.UnitState) {
 	}
 }
 
-func (p *UnitStatePublisher) addToCache(update *unit.UnitStateHeartbeat) {
+func (p *UnitStatePublisher) updateCache(update *unit.UnitStateHeartbeat) (changed bool) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
-	last := p.cache[update.Name]
+	last, ok := p.cache[update.Name]
 	p.cache[update.Name] = update.State
 
-	// As an optimization, publish changes as they flow in
-	if !reflect.DeepEqual(last, update.State) {
-		go p.publishOne(update.Name, update.State)
+	if !ok || !reflect.DeepEqual(last, update.State) {
+		changed = true
 	}
+
+	return
 }
 
 func (p *UnitStatePublisher) Purge() {
