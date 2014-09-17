c488786a200194a00ad56cae978d8513118c81f8
Brian Waldon <bcwaldon@gmail.com>
2014-09-17 15:47:41 -0700

    fleetctl: only call API once in status command

--- fleetctl/status.go
+++ fleetctl/status.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/coreos/fleet/job"
+	"github.com/coreos/fleet/schema"
 )
 
 var cmdStatusUnits = &Command{
@@ -23,35 +24,46 @@ fleetctl status myservice/*`,
 }
 
 func runStatusUnits(args []string) (exit int) {
-	for i, v := range args {
-		// This extra newline here to match systemctl status output
+	units, err := cAPI.Units()
+	if err != nil {
+		stderr("Error retrieving unit: %v", err)
+		return 1
+	}
+
+	uMap := make(map[string]*schema.Unit, len(args))
+	for _, u := range units {
+		u := u
+		uMap[u.Name] = u
+	}
+
+	names := make([]string, len(args))
+	for i, arg := range args {
+		name := unitNameMangle(arg)
+		names[i] = name
+
+		u, ok := uMap[name]
+
+		if !ok {
+			stderr("Unit %s does not exist.", name)
+			return 1
+		}
+
+		if job.JobState(u.CurrentState) == job.JobStateInactive {
+			stderr("Unit %s does not appear to be loaded.", name)
+			return 1
+		}
+	}
+
+	for i, name := range names {
+		// This extra newline is here to match systemctl status output
 		if i != 0 {
 			fmt.Printf("\n")
 		}
 
-		name := unitNameMangle(v)
-		exit = printUnitStatus(name)
-		if exit != 0 {
+		cmd := fmt.Sprintf("systemctl status -l %s", name)
+		if exit := runCommand(cmd, uMap[name].MachineID); exit != 0 {
 			break
 		}
 	}
 	return
-}
-
-func printUnitStatus(name string) int {
-	u, err := cAPI.Unit(name)
-	if err != nil {
-		stderr("Error retrieving unit %s: %v", name, err)
-		return 1
-	}
-	if u == nil {
-		stderr("Unit %s does not exist.", name)
-		return 1
-	} else if job.JobState(u.CurrentState) == job.JobStateInactive {
-		stderr("Unit %s does not appear to be running.", name)
-		return 1
-	}
-
-	cmd := fmt.Sprintf("systemctl status -l %s", name)
-	return runCommand(cmd, u.MachineID)
 }
