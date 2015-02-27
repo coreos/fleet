037e9996d85f4c4a9c66b891b7343acf533aa28d
Brian Waldon <brian@waldon.cc>
2015-02-27 14:09:01 -0800

    version: bump to v0.9.1

--- version/version.go
+++ version/version.go
@@ -20,7 +20,7 @@ import (
 	"github.com/coreos/fleet/Godeps/_workspace/src/github.com/coreos/go-semver/semver"
 )
 
-const Version = "0.9.0"
+const Version = "0.9.1"
 
 var SemVersion semver.Version
 
