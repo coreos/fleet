cb6eb8c49ae4e6d6ccd43d3ed1004d4d321b8f6b
Brian Waldon <brian@waldon.cc>
2015-04-29 08:42:39 -0700

    version: bump to v0.10.1+git

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ import (
 	"github.com/coreos/fleet/Godeps/_workspace/src/github.com/coreos/go-semver/semver"
 )
 
-const Version = "0.10.1"
+const Version = "0.10.1+git"
 
 var SemVersion semver.Version
 
