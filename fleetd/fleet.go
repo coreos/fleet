coreos/fleet

commit 32a51814a57e8337ed972fc31764a3d7d23d667d

fleetd: support the version command
Now support "fleetd version" and "fleetd --version"

Fixes #1210

fleetd/fleet.go
@@ -48,7 +48,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if *printVersion {
+	if *printVersion || userset.Args()[0] == "version" {
 		fmt.Println("fleetd version", version.Version)
 		os.Exit(0)
 	}

commit 8ebb81bcd1668be4a874db98cbf2e67ae305d7f6

fleetd: fix support for "fleetd version"
Commit 32a51814 broke fleetd, making "fleetd version" the *only* thing it
could do and causing a index error panic otherwise. Now in addition to
properly checking the number of unparsed args exit with a usage error if
given any other unknown arguments.

Simply exiting if there are any unparsed args would have been sufficient
to fix #1210 but on the other hand allowing "fleetd version" doesn't
hurt so might as well keep it.

Fixes #1214 and #1210

fleetd/fleet.go
@@ -48,7 +48,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	if *printVersion || userset.Args()[0] == "version" {
+	args := userset.Args()
+	if len(args) == 1 && args[0] == "version" {
+		*printVersion = true
+	} else if len(args) != 0 {
+		userset.Usage()
+		os.Exit(1)
+	}
+
+	if *printVersion {
 		fmt.Println("fleetd version", version.Version)
 		os.Exit(0)
 	}