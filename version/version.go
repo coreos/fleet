3d0b3d9136bfa671486a0501c75a6e3167181926
Nick Owens <mischief@offblast.org>
2015-07-22 22:34:56 -0700

    version: bump to v0.11.2+git

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ import (
 	"github.com/coreos/fleet/Godeps/_workspace/src/github.com/coreos/go-semver/semver"
 )
 
-const Version = "0.11.1+git"
+const Version = "0.11.2+git"
 
 var SemVersion semver.Version
 
