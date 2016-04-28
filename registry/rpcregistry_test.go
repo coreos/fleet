9829be538eeabfc4899ef728e73dde00ff6ba6f6
Hector Fernandez <hectorj@gmail.com>
2016-04-28 13:08:48 +0200

    Added rpcregistry test

---
+++ registry/rpcregistry_test.go
@@ -0,0 +1,34 @@
+package registry
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/coreos/fleet/Godeps/_workspace/src/google.golang.org/grpc"
+
+	pb "github.com/coreos/fleet/protobuf"
+)
+
+func TestRPCRegistryClientCreation(t *testing.T) {
+	lis, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatalf("Failed to listen: %v", err)
+	}
+	_, port, err := net.SplitHostPort(lis.Addr().String())
+	if err != nil {
+		t.Fatalf("Failed to parse listener address: %v", err)
+	}
+	addr := "localhost:" + port
+	conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithTimeout(5*time.Second), grpc.WithBlock())
+	if err != nil {
+		t.Fatalf("Failed to dial to the server %q: %v", addr, err)
+	}
+	// Close unaccepted connection (i.e., conn).
+	lis.Close()
+
+	registryClient := pb.NewRegistryClient(conn)
+	if registryClient == nil {
+		t.Fatalf("Failed to create a new grpc registry to the server %q: %v", addr, err)
+	}
+}
---
+++ registry/rpcutils_test.go
@@ -0,0 +1,92 @@
+package registry
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/coreos/fleet/job"
+	pb "github.com/coreos/fleet/protobuf"
+	"github.com/coreos/fleet/unit"
+)
+
+const (
+	TargetState_UNKNOWN pb.TargetState = 50
+)
+
+func TestRpcUnitStateToJobState(t *testing.T) {
+	got := rpcUnitStateToJobState(pb.TargetState_INACTIVE)
+	expect := job.JobStateInactive
+	if !reflect.DeepEqual(got, expect) {
+		t.Fatalf("got %#v, expected %#v", got, expect)
+	}
+
+	got = rpcUnitStateToJobState(pb.TargetState_LOADED)
+	expect = job.JobStateLoaded
+	if !reflect.DeepEqual(got, expect) {
+		t.Fatalf("got %#v, expected %#v", got, expect)
+	}
+
+	got = rpcUnitStateToJobState(pb.TargetState_LAUNCHED)
+	expect = job.JobStateLaunched
+	if !reflect.DeepEqual(got, expect) {
+		t.Fatalf("got %#v, expected %#v", got, expect)
+	}
+
+	// Unknown state
+	got = rpcUnitStateToJobState(TargetState_UNKNOWN)
+	expect = job.JobStateInactive
+	if !reflect.DeepEqual(got, expect) {
+		t.Fatalf("got %#v, expected %#v", got, expect)
+	}
+}
+
+func TestRpcUnitStateToExtUnitState(t *testing.T) {
+	want := &pb.UnitState{
+		Name:        "foo",
+		Hash:        "heh",
+		LoadState:   "foo",
+		ActiveState: "bar",
+		SubState:    "baz",
+		MachineID:   "machine1",
+	}
+	expect := &unit.UnitState{
+		UnitName:    "foo",
+		UnitHash:    "heh",
+		LoadState:   "foo",
+		ActiveState: "bar",
+		SubState:    "baz",
+		MachineID:   "machine1",
+	}
+
+	got := rpcUnitStateToExtUnitState(want)
+	if !reflect.DeepEqual(got, expect) {
+		t.Fatalf("got %#v, expected %#v", got, expect)
+	}
+}
+
+func TestRpcUnitToJobUnit(t *testing.T) {
+	contents := `
+[Unit]
+Description = Foo
+`
+	unitFile, err := unit.NewUnitFile(contents)
+	if err != nil {
+		t.Fatalf("Unexpected error parsing unit %q: %v", contents, err)
+	}
+
+	want := &pb.Unit{
+		Name:         "foo",
+		Unit:         unitFile.ToPB(),
+		DesiredState: pb.TargetState_LOADED,
+	}
+	expect := &job.Unit{
+		Name:        "foo",
+		Unit:        *unitFile,
+		TargetState: job.JobStateLoaded,
+	}
+
+	got := rpcUnitToJobUnit(want)
+	if !reflect.DeepEqual(got, expect) {
+		t.Fatalf("got %#v, expected %#v", got, expect)
+	}
+}
